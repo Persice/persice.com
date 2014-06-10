4dace167b67b42fb95562ad0600fa6a30cc203a8
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-06-10 08:31:48 +0300

    [BK-91]-Set up heroku environment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath bekindred bekindred.wsgi
--- bekindred/bekindred/wsgi.py
+++ bekindred/bekindred/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bekindred.settings")
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
