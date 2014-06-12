b78bff6b37a53946af25bf34e0392dbcdf9b0fd2
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-06-12 23:21:39 +0300

    prepare to deploy to heroku

--- bekindred/bekindred/settings/local.py
+++ bekindred/bekindred/settings/local.py
@@ -1,5 +1,9 @@
 from .base import *
 
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
---
+++ bekindred/bekindred/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
