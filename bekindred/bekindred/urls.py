0c9e987eae3548fd2deff5f40f985b511c4c92c6
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-06-12 22:03:02 +0300

    [BK-91] - Set up heroku environment - add user authenticated

--- bekindred/bekindred/urls.py
+++ bekindred/bekindred/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 from django.contrib import admin
@@ -6,7 +7,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', TemplateView.as_view(template_name="homepage.html")),
+                       url(r'^$', login_required(TemplateView.as_view(template_name="homepage.html"))),
                        url(r'^goal/', include('goals.urls')),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+                       url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
                        url(r'^admin/', include(admin.site.urls)),
 )
--- bekindred/goals/views.py
+++ bekindred/goals/views.py
@@ -1,20 +1,27 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Goal
 
 
-class GoalListView(ListView):
+class LoginRequiredMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+class GoalListView(LoginRequiredMixin, ListView):
     model = Goal
 
 
-class GoalCreate(CreateView):
+class GoalCreate(LoginRequiredMixin, CreateView):
     model = Goal
     fields = ['description']
     success_url = '/goal'
 
 
-class GoalUpdate(UpdateView):
+class GoalUpdate(LoginRequiredMixin, UpdateView):
     model = Goal
     fields = ['description']
     success_url = '/goal'
