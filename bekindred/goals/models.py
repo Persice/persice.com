395953e58cfe8ec6c84d9e2babd82d03039fd27e
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-06-13 22:34:07 +0300

    Create user model and add relationship with Goals model

--- bekindred/goals/models.py
+++ bekindred/goals/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Goal(models.Model):
     description = models.CharField(max_length=20, blank=False)
+    users = models.ManyToManyField(User)
+    is_offer = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.description
