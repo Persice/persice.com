e8528e14665923323a7738db390af7390e00ba22
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-07-28 23:45:51 +0300

    [BK-154] Match feed displays users based on distance.

--- bekindred/bekindred/settings/production.py
+++ bekindred/bekindred/settings/production.py
@@ -3,6 +3,7 @@ from os import environ
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+POSTGIS_VERSION = (2, 1, 0)
 GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
