d8468e6cc86bf2669fca8b734ef7acdfcc10466e
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-07-29 00:01:02 +0300

    [BK-154] Match feed displays users based on distance.

--- bekindred/bekindred/settings/production.py
+++ bekindred/bekindred/settings/production.py
@@ -13,6 +13,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DEBUG = False
 
