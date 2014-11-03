3e3f46d59d882d7c8765e5754990c7050229d07b
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-11-03 20:26:15 +0200

    [BK-225] Create model manager for matching interests

--- bekindred/interests/models.py
+++ bekindred/interests/models.py
@@ -1,13 +1,45 @@
 from django.db import models
 from django_facebook.models import FacebookCustomUser
+from djorm_pgfulltext.fields import VectorField
+from djorm_pgfulltext.models import SearchManager
+
+
+class InterestManager(models.Manager):
+    def search_interest(self, user_id):
+
+        user_interests = Interest.objects.filter(user=FacebookCustomUser.objects.get(pk=user_id))
+
+        interests = Interest.objects.exclude(id__in=user_interests.values_list('id', flat=True))
+        search_desc = []
+        for interest in user_interests:
+            search_desc.extend(interests.search(interest))
+
+        return search_desc
+
+    def search_interest_to_interest(self, exclude_friends, search_desc):
+        return list(Interest.objects.exclude(user_id__in=exclude_friends).
+                    filter(goal__in=search_desc).values_list('user', flat=True))
+
 
 
 class Interest(models.Model):
     description = models.CharField(max_length=50, null=False, blank=False)
     user = models.ForeignKey(FacebookCustomUser)
 
+    search_index = VectorField()
+
+    objects = SearchManager(
+        fields=('description', ),
+        config='pg_catalog.english',
+        search_field='search_index',
+        auto_update_search_field=True
+    )
+
+    search_subject = InterestManager()
+
     def __unicode__(self):
         return self.description
 
     class Meta:
         unique_together = ("user", "description")
+
--- bekindred/interests/tests.py
+++ bekindred/interests/tests.py
@@ -1,11 +1,35 @@
 from django.test import TestCase
+from django_facebook.models import FacebookCustomUser
 from interests.models import Interest
 
 
 class InterestsTestCase(TestCase):
     def setUp(self):
-        Interest.objects.create(description="test desc")
+        user = FacebookCustomUser.objects.create_user(
+            username='jacob', password='top_secret')
+        Interest.objects.create(description="test desc", user=user)
+        self.i1 = Interest.objects.create(description="reading", user=user)
 
     def test_interests_description(self):
         desc = Interest.objects.get(description="test desc").description
         self.assertEqual(desc, "test desc")
+
+    def test_search_interest(self):
+        self.assertEqual(Interest.objects.search('read')[0].description, 'reading')
+
+    def test_match_interests(self):
+        user = FacebookCustomUser.objects.create_user(
+            username='goro', password='top_secret')
+        user2 = FacebookCustomUser.objects.create_user(
+            username='tesla', password='top_secret')
+        user3 = FacebookCustomUser.objects.create_user(
+            username='guido', password='top_secret')
+        i1 = Interest.objects.create(description="interest1", user=user)
+        i2 = Interest.objects.create(description="interest2", user=user)
+        i3 = Interest.objects.create(description="read", user=user)
+        i4 = Interest.objects.create(description="read", user=user2)
+        i5 = Interest.objects.create(description="top int", user=user2)
+        i6 = Interest.objects.create(description="reading", user=user3)
+
+        search = Interest.search_subject.search_interest(user.id)
+        self.assertEqual([x.description for x in search], [u'reading', u'read', u'reading'])
