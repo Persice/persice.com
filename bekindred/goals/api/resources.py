d3d788dcd1d8dfe2b687578e61b99503d0bdf3dc
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-11-18 19:49:59 +0200

    update resources for goals

--- bekindred/goals/api/resources.py
+++ bekindred/goals/api/resources.py
@@ -1,17 +1,27 @@
+from tastypie import fields
 from tastypie.authentication import SessionAuthentication
-from tastypie.authorization import DjangoAuthorization
+from tastypie.authorization import DjangoAuthorization, Authorization
 from tastypie.constants import ALL
 from tastypie.resources import ModelResource
-from goals.models import Subject
+from goals.models import Subject, Goal
+from photos.api.resources import UserResource
 
 
-class SubjectResource(ModelResource):
+class GoalResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
     class Meta:
-        queryset = Subject.objects.all()
+        queryset = Goal.objects.all()
         fields = ['description']
         allowed_methods = ['get']
         filtering = {
             'description': ALL
         }
         authentication = SessionAuthentication()
-        authorization = DjangoAuthorization()
+        authorization = Authorization()
+
+    def get_object_list(self, request):
+        return super(GoalResource, self).get_object_list(request).filter(user_id=request.user.id)
+
+    def post_list(self, request, **kwargs):
+        return
