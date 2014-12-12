d16b1ee416fb8c503f9079001155f8e614a088c5
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-12-12 21:59:03 +0200

    [BK-307] Matching algorithm looks for similarities between users' Activities & Interests and Facebook likes.

--- bekindred/members/models.py
+++ bekindred/members/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import UserManager
 from django_facebook.models import FacebookCustomUser, FacebookLike
+from interests.models import Interest
 
 
 class ActiveManager(UserManager):
@@ -19,7 +20,7 @@ class FacebookLikeProxyManager(models.Manager):
     @staticmethod
     def match_fb_likes_to_fb_likes(user_id, exclude_friends):
         """
-        Return the list of matched user_ids
+        Return the list of matched Facebook user likes
         """
         u_likes = FacebookLike.objects.filter(user_id=user_id)
         target_likes = FacebookLike.objects.exclude(user_id__in=[user_id] + exclude_friends)
@@ -34,13 +35,21 @@ class FacebookLikeProxyManager(models.Manager):
         return match_likes
 
     @staticmethod
-    def match_activities_to_fb_likes(user_id):
-        pass
+    def match_interests_to_fb_likes(user_id, exclude_friends):
+        """
+        Return the list of matched Facebook user likes
+        """
+        u_interests = Interest.objects.filter(user_id=user_id)
+        target_likes = FacebookLike.objects.exclude(user_id__in=[user_id] + exclude_friends)
 
+        match_likes = []
+        for interest in u_interests:
+            # FTS extension by default uses plainto_tsquery instead of to_tosquery,
+            #  for this reason the use of raw parameter.
+            tsquery = ' | '.join(unicode(interest.description).split())
+            match_likes.extend(target_likes.search(tsquery, raw=True))
 
-    @staticmethod
-    def match_fb_likes_to_activities(user_id):
-        pass
+        return match_likes
 
 
 class FacebookLikeProxy(FacebookLike):
--- bekindred/members/tests/test_managers.py
+++ bekindred/members/tests/test_managers.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from django_facebook.models import FacebookLike, FacebookCustomUser
+from interests.models import Interest
 from members.models import FacebookLikeProxy
 from random import randint
 
@@ -11,26 +12,36 @@ def random_digits(digits):
 
 
 class TestFacebookLikeProxyManager(TestCase):
-    def setUp(self):
-        self.user = FacebookCustomUser.objects.create_user(username='user_a', password='test')
-        self.user1 = FacebookCustomUser.objects.create_user(username='user_b', password='test')
-        self.user2 = FacebookCustomUser.objects.create_user(username='user_c', password='test')
-        self.user3 = FacebookCustomUser.objects.create_user(username='user_d', password='test')
-        FacebookLike.objects.create(user_id=self.user.id, facebook_id=random_digits(10), name='Facebook Engineering')
-        FacebookLike.objects.create(user_id=self.user.id, facebook_id=random_digits(10), name='Kyiv Python User Group')
-        FacebookLike.objects.create(user_id=self.user.id, facebook_id=random_digits(10), name='Learn Ruby')
-        FacebookLike.objects.create(user_id=self.user1.id, facebook_id=random_digits(10), name='Facebook Developers')
-        FacebookLike.objects.create(user_id=self.user1.id, facebook_id=random_digits(10), name='Learn Python')
-        FacebookLike.objects.create(user_id=self.user1.id, facebook_id=random_digits(10), name='Learn Ruby')
-        FacebookLike.objects.create(user_id=self.user2.id, facebook_id=random_digits(10), name='Study Ruby')
+    @classmethod
+    def setUpClass(cls):
+        cls.user = FacebookCustomUser.objects.create_user(username='user_a', password='test')
+        FacebookLike.objects.create(user_id=cls.user.id, facebook_id=random_digits(10), name='Facebook Engineering')
+        FacebookLike.objects.create(user_id=cls.user.id, facebook_id=random_digits(10), name='Kyiv Python User Group')
+        FacebookLike.objects.create(user_id=cls.user.id, facebook_id=random_digits(10), name='Learn Ruby')
 
-    def tearDown(self):
-        pass
+        cls.user1 = FacebookCustomUser.objects.create_user(username='user_b', password='test')
+        FacebookLike.objects.create(user_id=cls.user1.id, facebook_id=random_digits(10), name='Facebook Developers')
+        FacebookLike.objects.create(user_id=cls.user1.id, facebook_id=random_digits(10), name='Learn Python')
+        FacebookLike.objects.create(user_id=cls.user1.id, facebook_id=random_digits(10), name='Learn Ruby')
+
+        cls.user2 = FacebookCustomUser.objects.create_user(username='user_c', password='test')
+        FacebookLike.objects.create(user_id=cls.user2.id, facebook_id=random_digits(10), name='Study Ruby')
+
+        cls.user3 = FacebookCustomUser.objects.create_user(username='user_d', password='test')
+
+        cls.user4 = FacebookCustomUser.objects.create_user(username='user_e', password='test')
+        Interest.objects.create(user_id=cls.user4.id, description='Likes Python')
+        Interest.objects.create(user_id=cls.user4.id, description='Likes Ruby')
 
     def test_match_fb_likes_to_fb_likes(self):
         r = FacebookLikeProxy.objects.match_fb_likes_to_fb_likes(self.user.id, [self.user1.id])
         self.assertEqual(len(r), 1)
         self.assertEquals(r[0].name, 'Study Ruby')
 
-    # def test_match_activities_to_fb_likes(self):
-    #     self.fail()
+    def test_exclude_friends_in_matched(self):
+        r = FacebookLikeProxy.objects.match_fb_likes_to_fb_likes(self.user2.id, [self.user.id, self.user1.id])
+        self.assertEqual(len(r), 0)
+
+    def test_match_interests_to_fb_likes(self):
+        l = FacebookLikeProxy.objects.match_interests_to_fb_likes(self.user4.id, [self.user3.id])
+        self.assertEqual(len(l), 5)
