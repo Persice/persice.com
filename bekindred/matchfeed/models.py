801d76013e363c1dc9e4a8afeeb6d5067769d633
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2015-01-13 20:58:25 +0200

    Fixed with match goals -> offers in matchedfeed

--- bekindred/matchfeed/models.py
+++ bekindred/matchfeed/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from friends.models import Friend, FacebookFriendUser
-from goals.models import Goal
+from goals.models import Goal, Offer
 from interests.models import Interest
 from members.models import FacebookLikeProxy
 from itertools import groupby
@@ -12,7 +12,7 @@ class MatchFeedManager(models.Manager):
         results = {'users': []}
         # calculate friends
         friends = Friend.objects.all_my_friends(user_id) + Friend.objects.thumbed_up_i(user_id) + \
-                  Friend.objects.deleted_friends(user_id)
+                  Friend.objects.deleted_friends(user_id) + [user_id]
                   # FacebookFriendUser.objects.all_my_friends(user_id) + \
 
         match_goals_to_goals = Goal.objects_search.match_goals_to_goals(user_id, friends)
@@ -20,20 +20,20 @@ class MatchFeedManager(models.Manager):
         match_goals = match_goals_to_goals | match_offers_to_goals
 
         goals = {}
-        for user_id, group in groupby(match_goals, lambda x: x.user_id):
-            goals[user_id] = []
+        for _user_id, group in groupby(match_goals, lambda x: x.user_id):
+            goals[_user_id] = []
             for thing in group:
-                goals[user_id].append(unicode(thing))
+                goals[_user_id].append(unicode(thing))
 
-        match_offers_to_offers = Goal.objects_search.match_goals_to_goals(user_id, friends)
-        match_goals_to_offers = Goal.objects_search.match_offers_to_goals(user_id, friends)
+        match_offers_to_offers = Offer.objects_search.match_offers_to_offers(user_id, friends)
+        match_goals_to_offers = Offer.objects_search.match_goals_to_offers(user_id, friends)
         match_offers = match_offers_to_offers | match_goals_to_offers
 
         offers = {}
-        for user_id, group in groupby(match_offers, lambda x: x.user_id):
-            offers[user_id] = []
+        for _user_id, group in groupby(match_offers, lambda x: x.user_id):
+            offers[_user_id] = []
             for thing in group:
-                offers[user_id].append(unicode(thing))
+                offers[_user_id].append(unicode(thing))
 
         match_likes_to_likes = FacebookLikeProxy.objects.match_fb_likes_to_fb_likes(user_id, friends)
         match_interests_to_likes = FacebookLikeProxy.objects.match_interests_to_fb_likes(user_id, friends)
@@ -41,10 +41,10 @@ class MatchFeedManager(models.Manager):
 
         # Match Likes
         likes = {}
-        for user_id, group in groupby(match_likes, lambda x: x.user_id):
-            likes[user_id] = []
+        for _user_id, group in groupby(match_likes, lambda x: x.user_id):
+            likes[_user_id] = []
             for thing in group:
-                likes[user_id].append(thing.name)
+                likes[_user_id].append(thing.name)
 
         match_interests_to_interests = Interest.search_subject.match_interests_to_interests(user_id, friends)
         match_likes_to_interests = Interest.search_subject.match_fb_likes_to_interests(user_id, friends)
@@ -52,10 +52,10 @@ class MatchFeedManager(models.Manager):
 
         # Match Interests
         interests = {}
-        for user_id, group in groupby(match_interests, lambda x: x.user_id):
-            interests[user_id] = []
+        for _user_id, group in groupby(match_interests, lambda x: x.user_id):
+            interests[_user_id] = []
             for thing in group:
-                interests[user_id].append(thing.description)
+                interests[_user_id].append(thing.description)
 
         matched_users = set(goals.keys() + offers.keys() + likes.keys() + interests.keys())
         for user in matched_users:
