8f2176cd3fb8a3f06794c96a56fb97c8e93cbf57
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2015-02-06 20:25:03 +0200

    [BK-411] Import/Export Users

--- bekindred/members/admin.py
+++ bekindred/members/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from django_facebook.models import FacebookCustomUser
+
+
+class FacebookCustomUserResource(resources.ModelResource):
+
+    class Meta:
+        model = FacebookCustomUser
+
+
+class FacebookCustomUserAdmin(ImportExportModelAdmin):
+    resource_class = FacebookCustomUserResource
+
+
+admin.site.register(FacebookCustomUser, FacebookCustomUserAdmin)
