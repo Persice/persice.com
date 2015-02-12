636bc1c03700ce6d4c7e334e73b2c4a83833fa2d
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-02-12 02:39:42 +0100

    [BK-422] Connect NodeJs socketio server with redis db

--- socketio/app.js
+++ socketio/app.js
@@ -1,34 +1,54 @@
 var express = require('express');
 
 var app = express();
+var cookie_reader = require('cookie');
+var querystring = require('querystring');
+var redis = require('redis');
+var redisAdapter = require('socket.io-redis');
 var port = 3000;
+
+
 
 var io = require('socket.io').listen(app.listen(port));
 
-var messages = [
-{
-    'title': 'The network is down!',
-    'desc': 'The network is down and I cannot perform my work'
-},
-{
-    'title': 'I lost my pen',
-    'desc': 'I was near my cube when I noticed that my pen was gone!  Someone must have taken it.'
-},
-{
-    'title': 'PC Load Letter',
-    'desc': 'I wish I knew what it meant, but the printer is not working.'
-},
-{
-    'title': 'My account is locked',
-    'desc': 'I know that my password was correct even though it failed.  So I kept trying.  Only about 50 times.'
-}
-];
+io.adapter(redisAdapter({
+    host: '127.0.0.1',
+    port: 6379
+}));
+
+
+//Configure socket.io to store cookie set by Django
+io.use(function(socket, next){
+
+    if(socket.request.headers.cookie){
+        socket.cookie = cookie_reader.parse(socket.request.headers.cookie);
+        console.log(socket.cookie);
+        return next(null, true);
+    }
+    next(new Error('Authentication error'));
+
+});
 
 io.on('connection', function(socket){
     console.log('a user connected');
 
+    // Create redis client
+    client = redis.createClient();
+
+    console.log(socket.cookie['sessionid']);
+
+    // Subscribe to the Redis events channel
+    client.subscribe('notifications.' + socket.cookie['sessionid']);
+
+    // Grab message from Redis and send to client
+    client.on('message', function(channel, message){
+        console.log('on message', message);
+        socket.send(message);
+    });
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        client.unsubscribe('notifications.' + socket.cookie['sessionid']);
     });
 
     socket.on('newmessage', function(){
