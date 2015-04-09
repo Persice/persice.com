d7dcdd2a9f1d90f7a1ea808678ad05ee1ae605e2
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-04-09 23:39:09 +0200

    [BK-528] Filter panel (right side)

--- bekindred/static/beKindredFrontend/src/components/rightsidebarfilter/filter.controller.js
+++ bekindred/static/beKindredFrontend/src/components/rightsidebarfilter/filter.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 angular.module('beKindred')
-.controller('FilterController', function ($scope, $rootScope, FiltersFactory, USER_ID, $log) {
+  .controller('FilterController', function($scope, $rootScope, FiltersFactory, USER_ID, $log) {
     $scope.distanceValue = 10000;
     $scope.distanceOptions = {
-        range: {min: 1, max: 10000}
+      range: {
+        min: 1,
+        max: 10000
+      }
     };
 
     $scope.genders = '';
@@ -12,13 +15,13 @@ angular.module('beKindred')
 
 
     $scope.defaultFilters = {
-        objects: [{
-            distance: 10000,
-            gender: 'm,f',
-            min_age: 25,
-            max_age: 60,
-            keyword: '',
-            user: '/api/v1/auth/user/' + USER_ID + '/'
+      objects: [{
+        distance: 10000,
+        gender: 'm,f',
+        min_age: 25,
+        max_age: 60,
+        keyword: '',
+        user: '/api/v1/auth/user/' + USER_ID + '/'
         }]
 
     };
@@ -29,132 +32,133 @@ angular.module('beKindred')
     $scope.showFilterMessage = false;
     $scope.filterID = null;
 
-    $scope.ageValues = [25,60];
+    $scope.ageValues = [25, 60];
     $scope.ageOptions = {
-        range: {min: 18, max: 115}
+      range: {
+        min: 18,
+        max: 115
+      }
     };
 
 
     $scope.myKeywords = [];
 
 
-    $scope.saveFilters = function () {
+    $scope.saveFilters = function() {
 
-        FiltersFactory.query( {
-            format: 'json'
-        }).$promise.then(function(response) {
-            $scope.filters = response.objects;
+      FiltersFactory.query({
+        format: 'json'
+      }).$promise.then(function(response) {
+        $scope.filters = response.objects;
 
-            $scope.filterID = $scope.filters[0].id;
+        $scope.filterID = $scope.filters[0].id;
 
-            $scope.newFilters = {
-                distance: $scope.distanceValue,
-                gender: $scope.genders,
-                min_age: $scope.ageValues[0],
-                max_age: $scope.ageValues[1],
-                keyword:  $scope.myKeywords.join(),
-                id: $scope.filterID,
-                user: '/api/v1/auth/user/' + USER_ID + '/'
-            };
+        $scope.newFilters = {
+          distance: $scope.distanceValue,
+          gender: $scope.genders,
+          min_age: $scope.ageValues[0],
+          max_age: $scope.ageValues[1],
+          keyword: $scope.myKeywords.join(),
+          id: $scope.filterID,
+          user: '/api/v1/auth/user/' + USER_ID + '/'
+        };
 
-            FiltersFactory.update({filterId: $scope.filterID}, $scope.newFilters,
-                function(success) {
-                }, function(error) {
-                });
+        FiltersFactory.update({
+            filterId: $scope.filterID
+          }, $scope.newFilters,
+          function(success) {},
+          function(error) {});
 
 
-        });
+      });
 
 
     };
 
     $scope.getFilters = function() {
-        FiltersFactory.query( {
-            format: 'json'
-        }).$promise.then(function(response) {
-            $scope.filters = response.objects;
+      FiltersFactory.query({
+        format: 'json'
+      }).$promise.then(function(response) {
+        $scope.filters = response.objects;
 
-            if ($scope.filters.length === 0) {
-                //API update filters - patch method
-                FiltersFactory.update({}, $scope.defaultFilters,
-                    function(success) {
-                        $('.ui.checkbox').checkbox('check');
-                    }, function(error) {
-                        console.log(error);
-                    });
-            }
-            else {
-                $scope.distanceValue = response.objects[0].distance;
-                $scope.ageValues[0] = response.objects[0].min_age;
-                $scope.ageValues[1] = response.objects[0].max_age;
-                if (response.objects[0].keyword !== '') {
-                    $scope.myKeywords = response.objects[0].keyword.split(',');
-                }
-                if (response.objects[0].gender === '') {
-                    $('.ui.checkbox').checkbox('uncheck');
-                }
-                if (response.objects[0].gender === 'm,f') {
-                    $('.ui.checkbox').checkbox('check');
-                }
-                if (response.objects[0].gender === 'm') {
-                    $('#male').checkbox('check');
-                    $('#female').checkbox('uncheck');
-                }
-                if (response.objects[0].gender === 'f') {
-                    $('#female').checkbox('check');
-                    $('#male').checkbox('uncheck');
-                }
-            }
-
-
-
-        });
-};
-
-$scope.getFilters();
+        if ($scope.filters.length === 0) {
+          //API update filters - patch method
+          FiltersFactory.update({}, $scope.defaultFilters,
+            function(success) {
+              $('.ui.checkbox').checkbox('check');
+            },
+            function(error) {
+              console.log(error);
+            });
+        } else {
+          $scope.distanceValue = response.objects[0].distance;
+          $scope.ageValues[0] = response.objects[0].min_age;
+          $scope.ageValues[1] = response.objects[0].max_age;
+          if (response.objects[0].keyword !== '') {
+            $scope.myKeywords = response.objects[0].keyword.split(',');
+          }
+          if (response.objects[0].gender === '') {
+            $('.ui.checkbox').checkbox('uncheck');
+          }
+          if (response.objects[0].gender === 'm,f') {
+            $('.ui.checkbox').checkbox('check');
+          }
+          if (response.objects[0].gender === 'm') {
+            $('#male').checkbox('check');
+            $('#female').checkbox('uncheck');
+          }
+          if (response.objects[0].gender === 'f') {
+            $('#female').checkbox('check');
+            $('#male').checkbox('uncheck');
+          }
+        }
 
 
 
+      });
+    };
 
-$scope.removeKeyword = function(index) {
-    $scope.myKeywords.splice(index, 1);
-    $scope.saveFilters();
-};
+    $scope.getFilters();
 
-$scope.addKeyword = function(item) {
-    $scope.filtermessage = '';
-    if (item == '' || item == undefined) {
+
+
+    $scope.removeKeyword = function(index) {
+      $scope.myKeywords.splice(index, 1);
+      $scope.saveFilters();
+    };
+
+    $scope.addKeyword = function(item) {
+      console.log('filtering');
+      $scope.filtermessage = '';
+      if (item == '' || item == undefined) {
         $scope.filtermessage = 'Keyword must have at least 3 letters';
         $scope.showFilterMessage = true;
-    }
-    else {
+      } else {
 
         if ($scope.myKeywords.length < 5) {
-            $scope.existing = false;
-            for (var i = $scope.myKeywords.length - 1; i >= 0; i--) {
-                if ( $scope.myKeywords[i] === item ) {
-                    $scope.existing = true;
-                }
+          $scope.existing = false;
+          for (var i = $scope.myKeywords.length - 1; i >= 0; i--) {
+            if ($scope.myKeywords[i] === item) {
+              $scope.existing = true;
             }
+          }
 
 
-            if ($scope.existing === false) {
-                $scope.myKeywords.push(item);
-                $scope.saveFilters();
-                $scope.newKeyword = '';
-                $scope.showFilterMessage = false;
-            }
-            else {
-                $scope.filtermessage = 'Keyword already exists';
-                $scope.showFilterMessage = true;
-            }
-        }
-        else {
-            $scope.filtermessage = 'You cannot add more than 5 keywords';
+          if ($scope.existing === false) {
+            $scope.myKeywords.push(item);
+            $scope.saveFilters();
+            $scope.newKeyword = '';
+            $scope.showFilterMessage = false;
+          } else {
+            $scope.filtermessage = 'Keyword already exists';
             $scope.showFilterMessage = true;
+          }
+        } else {
+          $scope.filtermessage = 'You cannot add more than 5 keywords';
+          $scope.showFilterMessage = true;
         }
-    }
+      }
 
-};
+    };
 
-});
+  });
