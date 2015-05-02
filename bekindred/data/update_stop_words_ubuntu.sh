122fda759024b0edd6680ab7de4a5e37ad443745
Andrey Soldatenko <a_soldatenko@wargaming.net>
2015-05-02 13:23:32 +0300

    Added auto update postgres stop.words

--- bekindred/data/update_stop_words_ubuntu.sh
+++ bekindred/data/update_stop_words_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # turn on debug mode
-#set -x
+set -x
 
 echo 'Update Postgres english.stop'
 echo '/usr/share/postgresql/9.3/tsearch_data/english.stop'
@@ -10,10 +10,13 @@ ORIGINAL_STOP_WORDS_COUNT=`cat stop.words | wc -l`
 
 echo 'Count stop words before updating: '${STOP_WORDS_COUNT}
 
-if [ ${STOP_WORDS_COUNT} -ne ORIGINAL_STOP_WORDS_COUNT ];
+if [ ${STOP_WORDS_COUNT} -ne ${ORIGINAL_STOP_WORDS_COUNT} ];
 then
     cp /usr/share/postgresql/9.3/tsearch_data/english.stop /usr/share/postgresql/9.3/tsearch_data/english.stop.default
     cat stop.words >> /usr/share/postgresql/9.3/tsearch_data/english.stop;
+    cat /usr/share/postgresql/9.3/tsearch_data/english.stop | sort -u > stop.uniq;
+    cat stop.uniq > /usr/share/postgresql/9.3/tsearch_data/english.stop;
+    rm stop.uniq;
     echo 'Count stop words after updating: '`cat /usr/share/postgresql/9.3/tsearch_data/english.stop | wc -l`;
     echo 'Restarting postgresql...';
     /etc/init.d/postgresql restart;
