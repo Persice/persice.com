d0e5012fc932ce903ea0dba992a2184d7e7a552f
Andrey Soldatenko <a_soldatenko@wargaming.net>
2015-05-04 18:03:47 +0300

    [BK-600] Admin users can see geolocation data within Admin panel

--- bekindred/world/admin.py
+++ bekindred/world/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from world.models import UserLocation
 
-# Register your models here.
+
+class UserLocationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserLocation, UserLocationAdmin)
