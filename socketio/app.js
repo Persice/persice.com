cf2cb26857dfca0a54c0572880a310da64622d20
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-05-23 14:04:00 +0200

    [BK-706] NodeJS Socket.io - Read new connection notification from Redis db and send it to the user

--- socketio/app.js
+++ socketio/app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server, {
-  origins: '*:*'
+    origins: '*:*'
 });
 
 var cookie_reader = require('cookie');
@@ -21,50 +21,59 @@ io.set("polling duration", 10);
 io.set("close timeout", 3);
 
 io.adapter(redisAdapter({
-  host: '127.0.0.1',
-  port: 6379
+    host: '127.0.0.1',
+    port: 6379
 }));
 
 //Configure socket.io to store cookie set by Django
 io.use(function(socket, next) {
 
-  if (socket.request.headers.cookie) {
-    socket.cookie = cookie_reader.parse(socket.request.headers.cookie);
-    return next(null, true);
-  }
-  next(new Error('Authentication error'));
+    if (socket.request.headers.cookie) {
+        socket.cookie = cookie_reader.parse(socket.request.headers.cookie);
+        return next(null, true);
+    }
+    next(new Error('Authentication error'));
 
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+    console.log('a user connected');
 
-  // Create redis client
-  client = redis.createClient();
+    // Create redis client
+    client = redis.createClient();
 
-  console.log(socket.cookie['sessionid']);
-  console.log(socket.cookie['userid']);
+    console.log(socket.cookie['sessionid']);
+    console.log(socket.cookie['userid']);
 
-  // Subscribe to the Redis events channel
-  client.subscribe('message.' + socket.cookie['userid']);
+    // Subscribe to the Redis events channel for new messages
+    client.subscribe('message.' + socket.cookie['userid']);
 
-  // Grab message from Redis and send to client
-  client.on('message', function(channel, message) {
-    console.log('on message ' + socket.cookie['userid'], message);
-    socket.send(message);
-  });
+    // Subscribe to the Redis events channel for new connections
+    client.subscribe('connection.' + socket.cookie['userid']);
 
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-    client.unsubscribe('message.' + socket.cookie['userid']);
-  });
+    // Grab message from Redis and send to client
+    client.on('message', function(channel, message) {
+        console.log('on message ' + socket.cookie['userid'], message);
+        socket.send(message);
+    });
 
-  socket.on('newmessage', function() {
-    console.log('new message');
-  });
+    // Grab new connection from Redis and send to client
+    client.on('connection', function(channel, message) {
+        console.log('on connection ' + socket.cookie['userid'], message);
+        socket.send(message);
+    });
 
-  // socket.on('notification', function(msg){
-  //     console.log('message: ' + msg.body);
-  //     socket.broadcast.emit('newmessage', msg);
-  // });
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+        client.unsubscribe('message.' + socket.cookie['userid']);
+    });
+
+    socket.on('newmessage', function() {
+        console.log('new message');
+    });
+
+    // socket.on('notification', function(msg){
+    //     console.log('message: ' + msg.body);
+    //     socket.broadcast.emit('newmessage', msg);
+    // });
 });
