ac330af313e07f237541c917bfc83a862fd2199b
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-06-04 01:56:54 +0200

    [BK-778] Desktop - Save event

--- bekindred/static/beKindredFrontend/src/components/modal/eventModal.directive.js
+++ bekindred/static/beKindredFrontend/src/components/modal/eventModal.directive.js
@@ -49,10 +49,8 @@
      * @desc controller for modal directive
      * @ngInject
      */
-    function EventModalController($scope, $log) {
+    function EventModalController($scope, USER_ID, EventsFactory, $state, $rootScope, $log) {
         var vm = this;
-
-        vm.saveEvent = saveEvent;
 
         vm.placeholder = {
             name: '',
@@ -69,8 +67,74 @@
             attachments: ''
         };
 
+        vm.showError = false;
+        vm.showSuccess = false;
+        vm.errorMessage = [];
+        vm.event = {
+            user: '/api/v1/auth/user/' + USER_ID + '/',
+            description: '',
+            ends_on: '',
+            location: '',
+            name: '',
+            repeat: '',
+            starts_on: '',
+            street: 'China Camp State Park',
+            city: 'San Rafael',
+            zipcode: 94941,
+            state: 'CA'
+        };
+
+        vm.saveEvent = saveEvent;
+        vm.resetForm = resetForm;
+
+        $rootScope.$on('saveEvent', function() {
+            $log.info('saveEvent');
+            vm.saveEvent();
+        });
+
         function saveEvent() {
-            $log.info('Saving event from modal');
+            vm.showError = false;
+            vm.showSuccess = false;
+            if (vm.event.description === '' || vm.event.ends_on === '' || vm.event.location === '' || vm.event.name === '' || vm.event.starts_on === '' || vm.event.repeat === '') {
+                vm.showError = true;
+                vm.errorMessage = ['All fields are required.'];
+            } else {
+                $log.info('started saving event');
+                vm.showError = false;
+                vm.showSuccess = false;
+                EventsFactory.save({}, vm.event,
+                    function(success) {
+                        vm.showError = false;
+                        $rootScope.$broadcast('closeModalCreateEvent');
+                        vm.resetForm();
+                    },
+                    function(error) {
+                        vm.errorMessage = [];
+                        vm.errorShow = true;
+                        if (error.data.event) {
+                            vm.errorMessage = error.data.event.error;
+                        }
+
+                    });
+            }
+
+        }
+
+
+        function resetForm() {
+            vm.event = {
+                user: '/api/v1/auth/user/' + USER_ID + '/',
+                description: '',
+                ends_on: '',
+                location: '',
+                name: '',
+                repeat: '',
+                starts_on: '',
+                street: 'China Camp State Park',
+                city: 'San Rafael',
+                zipcode: 94941,
+                state: 'CA'
+            };
         }
 
 
