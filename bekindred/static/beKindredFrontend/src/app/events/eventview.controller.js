07141d327edf549f6c88b4f8a9435cf317142223
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-06-17 04:48:30 +0200

    [ICE-831] Display Timezone name when viewing event
    mobile

--- bekindred/static/beKindredFrontend/src/app/events/eventview.controller.js
+++ bekindred/static/beKindredFrontend/src/app/events/eventview.controller.js
@@ -43,35 +43,6 @@
             $log.info('getting event: ' + eventId);
 
 
-            //mock get event
-
-
-            // vm.event = {
-            //     city: 'Zagreb',
-            //     description: 'asas',
-            //     ends_on: '2015-06-15T06:30:00',
-            //     id: 3,
-            //     location: '45.8275963,15.92102090000003',
-            //     name: 'sa',
-            //     repeat: 'w',
-            //     resource_uri: '/api/v1/event/3/',
-            //     starts_on: '2015-06-15T03:00:00',
-            //     state: 'HR',
-            //     street: 'Grgićev put ',
-            //     zipcode: 10000
-            // };
-
-
-            // vm.eventLocation = vm.event.street + ', ' + vm.event.city + ', ' + vm.event.zipcode + ' ' + vm.event.state;
-
-            // if (vm.event.location !== '0,0') {
-            //     vm.mapurl = 'https://www.google.com/maps/search/' + encodeURIComponent(vm.eventLocation) + '/@' + vm.event.location + ',15z';
-            //     vm.mapurlTrue = true;
-            // } else {
-            //     vm.mapurlTrue = false;
-            // }
-
-
 
             vm.loadingEvent = true;
             EventsFactory.query({
@@ -94,7 +65,8 @@
                 vm.starts_on_date = moment.utc(vm.event.starts_on, moment.ISO_8601).local().format('dddd, MMMM D, YYYY');
                 vm.ends_on_date = moment.utc(vm.event.ends_on, moment.ISO_8601).local().format('dddd, MMMM D, YYYY');
                 vm.starts_on_time = moment.utc(vm.event.starts_on, moment.ISO_8601).local().format('H:mm A');
-                vm.ends_on_time = moment.utc(vm.event.ends_on, moment.ISO_8601).local().format('H:mm A  UTCZ');
+                vm.ends_on_time = moment.utc(vm.event.ends_on, moment.ISO_8601).local().format('H:mm A ') + moment.tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format('z');
+
 
 
                 if (vm.ends_on_date !== vm.starts_on_date) {
