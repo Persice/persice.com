932c8fbe58cc32b52289cf6a5aa41fd60db02b2d
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-07-20 17:49:29 +0200

    Gulp watch task - put builddev after watching

--- bekindred/static/appFrontend/gulp/watch.js
+++ bekindred/static/appFrontend/gulp/watch.js
@@ -6,9 +6,9 @@ var paths = gulp.paths;
 
 gulp.task('watch', ['inject'], function() {
     gulp.watch([
-        paths.src + '/*.html',
+        paths.src + '/**/*.html',
         paths.src + '/{app,components}/**/*.scss',
         paths.src + '/{app,components}/**/*.js',
         'bower.json'
-    ], ['inject']);
+    ], ['builddev']);
 });
