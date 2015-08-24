817c3d6020f851824227f97416390addb837f816
Andrey Soldatenko <a_soldatenko@wargaming.net>
2015-08-24 11:20:25 +0300

    Added scripts for postgre upgrade

---
+++ upgrade_posgres_9.3_to_9.4_ubuntu_14.04.sh
@@ -0,0 +1,11 @@
+## Problem
+### postgresql-9.4-postgis-2.1 : Depends: libgdal1 (>= 1.9.0) but it is not going to be installed
+
+
+ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
+
+ sudo apt-get update && sudo apt-get upgrade
+
+ sudo apt-get install postgresql-9.4 postgresql-server-dev-9.4 postgresql-contrib-9.4
+ sudo apt-get install postgresql-9.4-postgis-2.1 pgadmin3 postgresql-contrib
+
