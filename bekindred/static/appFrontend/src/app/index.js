c606d0d5c0e62901bc4c20306097553024e46e6a
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-09-11 15:47:01 +0200

    Added errSrc directive for event images

--- bekindred/static/appFrontend/src/app/index.js
+++ bekindred/static/appFrontend/src/app/index.js
@@ -886,4 +886,15 @@ angular
                 });
             }
         };
-    }]);
+    }])
+    .directive('errSrc', function() {
+        return {
+            link: function(scope, element, attrs) {
+                element.bind('error', function() {
+                    if (attrs.src != attrs.errSrc) {
+                        attrs.$set('src', attrs.errSrc);
+                    }
+                });
+            }
+        }
+    });
