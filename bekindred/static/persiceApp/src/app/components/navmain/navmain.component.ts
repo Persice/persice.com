e96a55ecb8d60b45c0f169fce67ea20c30397f60
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-10-02 12:58:49 +0200

    ICE-1118 Finished left menu component

--- bekindred/static/persiceApp/src/app/components/navmain/navmain.component.ts
+++ bekindred/static/persiceApp/src/app/components/navmain/navmain.component.ts
@@ -1,16 +1,24 @@
 /// <reference path="../../../typings/_custom.d.ts" />
 
 import {Component, View, Directive, ElementRef} from 'angular2/angular2';
+import { RouterOutlet, RouterLink, Location, Router} from 'angular2/router';
 
 let view = require('./navmain.html');
 @Component({
   selector: 'nav-main'
 })
 @View({
-  template: view
+  template: view,
+  directives: [RouterOutlet, RouterLink]
 })
 export class NavMainComponent {
-  constructor() {
-
+  location: Location;
+  router: Router;
+  constructor(router: Router, location: Location) {
+    this.router = router;
+    this.location = location;
+  }
+  isLinkActive(path) {
+    return this.location.path() === path;
   }
 }
