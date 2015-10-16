019c096322407d2d4785444f2ab3de662eca3c76
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-10-17 00:59:25 +0200

    Removed unnecessary jquery code from init.js

--- bekindred/static/persiceApp/src/public/js/init.js
+++ bekindred/static/persiceApp/src/public/js/init.js
@@ -4,15 +4,6 @@
 $(document).ready(function() {
   'use strict';
 
-  // $('.match-percent').circleProgress({
-  //   value: 0.75,
-  //   size: 128,
-  //   thickness: 2,
-  //   fill: {
-  //     color: '#6dcbe8'
-  //   }
-  // });
-
   $('.match-percent-large').circleProgress({
     value: 0.75,
     size: 246,
@@ -22,11 +13,6 @@ $(document).ready(function() {
     }
   });
 
-  //Filter toggle for small screens
-  // $('.js-filter-toggle').on('click', function(e) {
-  //   e.preventDefault();
-  //   $('body').toggleClass('filter-is-open');
-  // });
 
   //Sick slider
   $('.match-profile__avatar').slick({
@@ -66,72 +52,24 @@ $(document).ready(function() {
     console.log('Modal is opened');
   });
 
-  // //Search drop prototype
-  // $('.search__input').focus(function() {
-  //   $(this)
-  //     .closest('.search')
-  //     .find('.has-search__drop')
-  //     .addClass('is-visible');
-  // });
-  // $('.search__input').focusout(function() {
-  //   $(this)
-  //     .closest('.search')
-  //     .find('.has-search__drop')
-  //     .removeClass('is-visible');
-  // });
 
-  //Select replacement
-  // $('select').minimalect();
-
-  //Profile Drop
-  // $('.profile-drop__arrow').click(function() {
-  //   $('.profile-drop__expand').slideToggle();
-  // });
 
   //Multiple line content ellipsis
   $('.truncate').dotdotdot({
     watch: 'window'
   });
 
-  //Range slider
-  $('.range').ionRangeSlider({
-    // jscs:disable
-    hide_min_max: true,
-    // jscs:enable
-    keyboard: true,
-    min: 0,
-    max: 5000,
-    from: 1000,
-    to: 4000,
-    type: 'double',
-    step: 1
-  });
-
-  $('.range-to').ionRangeSlider({
-    // jscs:disable
-    hide_min_max: true,
-    // jscs:enable
-    keyboard: true
-  });
-
-  //INIT IMGLIQUID used for images streching
-  // $('.js-image-liquid').imgLiquid({
-  //   fill: true,
-  //   horizontalAlign: 'center',
-  //   verticalAlign: 'center'
-  // });
-
   //TABS
   $('.event-photo-map__switch li').click(function() {
     var tabId = $(this).attr('data-tab');
     $(this)
-      .closest('.event-photo-map')
-      .find('.event-photo-map__switch li')
-      .removeClass('is-current');
+    .closest('.event-photo-map')
+    .find('.event-photo-map__switch li')
+    .removeClass('is-current');
     $(this)
-      .closest('.event-photo-map')
-      .find('.tab-content')
-      .removeClass('is-current');
+    .closest('.event-photo-map')
+    .find('.tab-content')
+    .removeClass('is-current');
     $(this).addClass('is-current');
     $('#' + tabId).addClass('is-current');
   });
