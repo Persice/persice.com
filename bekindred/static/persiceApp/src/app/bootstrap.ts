62c83241dba0cfd5878759d9915eefcff8172bb2
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-10-18 20:15:11 +0200

    Added frontend build for sprint_56

--- bekindred/static/persiceApp/src/app/bootstrap.ts
+++ bekindred/static/persiceApp/src/app/bootstrap.ts
@@ -5,38 +5,22 @@ import {bind, bootstrap, provide} from 'angular2/angular2';
 
 
 /*
- * Common Injectables
- * our custom helper injectables to configure our app differently using the dependency injection system
- */
-// import {
-//   JIT_CHANGEDETECTION_BINDINGS,
-//   DYNAMIC_CHANGEDETECTION_BINDINGS,
-//   PREGENERATED_CHANGEDETECTION_BINDINGS,
-//   BEST_CHANGEDETECTION_BINDINGS
-// } from '../bindings/change_detection_bindings';
-
-import {
-PATH_LOCATION_BINDINGS,
-HASH_LOCATION_BINDINGS
-} from '../bindings/location_bindings';
-
-/*
  * Angular Modules
  */
-import {FORM_BINDINGS} from 'angular2/angular2';
+import {FORM_PROVIDERS, ELEMENT_PROBE_PROVIDERS} from 'angular2/angular2';
 import {
-ROUTER_BINDINGS,
+ROUTER_PROVIDERS,
 ROUTER_PRIMARY_COMPONENT,
 HashLocationStrategy,
 LocationStrategy
 } from 'angular2/router';
-import {HTTP_BINDINGS} from 'angular2/http';
+import {HTTP_PROVIDERS} from 'angular2/http';
 
 /*
  * App Services
  * our collection of injectables services
  */
-import {APP_SERVICES_BINDINGS} from './services/services';
+import {APP_SERVICES_PROVIDERS} from './services/services';
 
 /*
  * App Component
@@ -48,29 +32,28 @@ import {AppComponent} from './components/app.component';
 /*
  * Universal injectables
  */
-const UNIVERSAL_BINDINGS = [
-  ROUTER_BINDINGS,
-  FORM_BINDINGS,
-  HTTP_BINDINGS,
-  APP_SERVICES_BINDINGS
+const UNIVERSAL_PROVIDERS = [
+  ELEMENT_PROBE_PROVIDERS,
+  ROUTER_PROVIDERS,
+  FORM_PROVIDERS,
+  HTTP_PROVIDERS,
+  APP_SERVICES_PROVIDERS
 ];
 
 /*
  * Platform injectables
  */
-const PLATFORM_BINDINGS = [
-// if we want to explicit change detection
-// BEST_CHANGEDETECTION_BINDINGS,
-  HASH_LOCATION_BINDINGS,
+const PLATFORM_PROVIDERS = [
+  provide(LocationStrategy, { useClass: HashLocationStrategy }),
+  provide(ROUTER_PRIMARY_COMPONENT, { useValue: AppComponent })
 ];
 
-const APP_BINDINGS = [
-  UNIVERSAL_BINDINGS,
-  PLATFORM_BINDINGS,
-  provide(ROUTER_PRIMARY_COMPONENT, { useValue: AppComponent })
+const APP_PROVIDERS = [
+  UNIVERSAL_PROVIDERS,
+  PLATFORM_PROVIDERS
 ];
 
 bootstrap(
   AppComponent,
-  APP_BINDINGS
+  APP_PROVIDERS
 );
