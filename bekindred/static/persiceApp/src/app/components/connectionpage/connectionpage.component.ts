8062c8a356a0462c71fe9f08f0b82cb1eaf02a53
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-10-20 13:37:49 +0200

    ICE-1135 Added Connections page

--- bekindred/static/persiceApp/src/app/components/connectionpage/connectionpage.component.ts
+++ bekindred/static/persiceApp/src/app/components/connectionpage/connectionpage.component.ts
@@ -1,16 +1,117 @@
 /// <reference path="../../../typings/_custom.d.ts" />
 
-import {Component} from 'angular2/angular2';
-import {RouterLink } from 'angular2/router';
+import {Component, NgIf, Inject} from 'angular2/angular2';
+import {Http, Headers, Response, HTTP_BINDINGS} from 'angular2/http';
+import {RouteParams} from 'angular2/router';
+
+import {UsersListComponent} from '../userslist/userslist.component';
+import {LoadingIndicatorComponent} from '../loadingindicator/loadingindicator.component';
+import {FilterComponent} from '../filter/filter.component';
+
+import {ConnectionsService} from '../../services/connections.service';
 
 let view = require('./connectionpage.html');
+
+declare var jQuery: any;
+
 @Component({
   selector: 'connection-page',
   template: view,
-  directives: [RouterLink]
+  directives: [
+    NgIf,
+    FilterComponent,
+    UsersListComponent,
+    LoadingIndicatorComponent
+  ]
 })
 export class ConnectionPageComponent {
-  constructor() {
+  items: Array<any> = [];
+  loading: boolean = false;
+  isListEmpty: boolean = false;
+  limit: number = 6;
+  filter: boolean = true;
+  next: string = '';
+  total_count: number = 0;
+  offset: number = 0;
+
+  constructor(
+    public service: ConnectionsService
+  ) {
 
   }
+
+
+  onInit() {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    this.getList();
+  }
+
+  getList() {
+    if (this.next === null) return;
+    this.loading = true;
+    this.service.get(this.next, this.limit, 'v1', this.filter)
+      .map(res => res.json())
+      .subscribe(data => this.assignList(data));
+  }
+
+
+  refreshList(event) {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    this.items = [];
+    this.isListEmpty = false;
+    this.next = '';
+    this.getList();
+  }
+
+  assignList(data) {
+
+    this.loading = false;
+
+    this.total_count = data.meta.total_count;
+    if (this.total_count === 0) {
+      this.isListEmpty = true;
+      return;
+    } else {
+      this.isListEmpty = false;
+    }
+
+
+    if (this.items.length > 0) {
+      let more = data.objects;
+      for (var i = more.length - 1; i >= 0; i--) {
+        this.items.push(more[i]);
+      }
+
+    }
+    else {
+      this.items = data.objects;
+    }
+
+    this.next = data.meta.next;
+    this.offset = data.meta.offset;
+
+
+    //bind to scroll event to load more data on bottom scroll
+    if (this.next !== null) {
+      jQuery(window).bind('scroll', this.handleScrollEvent.bind(this));
+    }
+    else {
+      jQuery(window).unbind('scroll');
+    }
+
+
+  }
+
+  handleScrollEvent(event) {
+    let scrollOffset = jQuery(window).scrollTop();
+    let threshold = jQuery(document).height() - jQuery(window).height() - 60;
+
+    if (this.next && scrollOffset > threshold) {
+      if (!this.loading) {
+        this.getList();
+      }
+    }
+
+  }
+
 }
