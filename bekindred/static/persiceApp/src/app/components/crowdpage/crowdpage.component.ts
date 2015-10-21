ae9b51720338028b430c71a20a6a274ba1bbe370
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-10-21 10:53:25 +0200

    ICE-1160 Implemented lazy loading for Crowd page

--- bekindred/static/persiceApp/src/app/components/crowdpage/crowdpage.component.ts
+++ bekindred/static/persiceApp/src/app/components/crowdpage/crowdpage.component.ts
@@ -5,84 +5,117 @@ import {Http, Headers, Response, HTTP_BINDINGS} from 'angular2/http';
 import {RouteParams} from 'angular2/router';
 
 import {UsersListComponent} from '../userslist/userslist.component';
-import {LoadingIndicatorComponent} from '../loadingindicator/loadingindicator.component';
+import {LoadingComponent} from '../loading/loading.component';
 import {FilterComponent} from '../filter/filter.component';
-import {FilterModel, InterfaceFilter} from '../../models/filter.model';
+
+import {CrowdService} from '../../services/crowd.service';
 
 let view = require('./crowdpage.html');
 
-const defaultFilters: InterfaceFilter = {
-  distance: 10000,
-  distance_unit: 'miles',
-  keyword: '',
-  gender: 'm,f',
-  min_age: '25',
-  max_age: '60',
-  order_criteria: 'match_score'
-};
-
-
+declare var jQuery: any;
 
 @Component({
   selector: 'crowd-page',
   template: view,
-  directives: [NgIf, FilterComponent, UsersListComponent, LoadingIndicatorComponent]
+  providers: [CrowdService],
+  directives: [
+    NgIf,
+    FilterComponent,
+    UsersListComponent,
+    LoadingComponent
+  ]
 })
 export class CrowdPageComponent {
   version: string = 'v2';
-  crowd: Array<any>;
-  filters: FilterModel;
-  loading: boolean;
-  isListEmpty: boolean;
-  constructor(public http: Http, @Inject(RouteParams) params: RouteParams) {
+  items: Array<any> = [];
+  loading: boolean = false;
+  isListEmpty: boolean = false;
+  limit: number = 9;
+  filter: boolean = true;
+  next: string = '';
+  total_count: number = 0;
+  offset: number = 0;
+
+  constructor(
+    @Inject(RouteParams) params: RouteParams,
+    public service: CrowdService
+  ) {
     this.version = params.get('version');
+
   }
+
 
   onInit() {
-    // set initial filters for Crowd page
-    this.filters = new FilterModel(defaultFilters);
-
-    //get Crowd data
-    this.getCrowd();
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    this.getList();
   }
 
-  getCrowd() {
-    this.crowd = [];
-    this.isListEmpty = false;
+  getList() {
+    if (this.next === null) return;
     this.loading = true;
-
-    //set api version
-    let apiUrl = '/api/v1/matchfeed2/?format=json&filter=true';
-
-    if (this.version === 'v1') {
-      apiUrl = '/api/v1/matchfeed/?format=json&filter=true';
-    }
-    if (this.version === 'v2') {
-      apiUrl = '/api/v1/matchfeed2/?format=json&filter=true';
-    }
-
-
-    // Crowd page load users list
-    this.http.get(apiUrl)
+    this.service.get(this.next, this.limit, this.version, this.filter)
       .map(res => res.json())
-      .subscribe(data => this.assignCrowd(data));
+      .subscribe(data => this.assignList(data));
   }
 
 
-  refreshCrowd(event) {
-    this.getCrowd();
+  refreshList(event) {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    this.items = [];
+    this.isListEmpty = false;
+    this.next = '';
+    this.getList();
   }
 
-  assignCrowd(data) {
+  assignList(data) {
+
     this.loading = false;
-    this.crowd = data.objects;
-    if (this.crowd.length === 0 ) {
+
+    this.total_count = data.meta.total_count;
+    if (this.total_count === 0) {
       this.isListEmpty = true;
+      return;
     } else {
       this.isListEmpty = false;
     }
 
 
+    if (this.items.length > 0) {
+      let more = data.objects;
+      for (var i = more.length - 1; i >= 0; i--) {
+        this.items.push(more[i]);
+      }
+
+    }
+    else {
+      this.items = data.objects;
+    }
+
+    this.next = data.meta.next;
+    this.offset = data.meta.offset;
+
+
+    //bind to scroll event to load more data on bottom scroll
+    if (this.next !== null) {
+      jQuery(window).bind('scroll', this.handleScrollEvent.bind(this));
+    }
+    else {
+      jQuery(window).unbind('scroll');
+    }
+
 
   }
+
+  handleScrollEvent(event) {
+    let scrollOffset = jQuery(window).scrollTop();
+    let threshold = jQuery(document).height() - jQuery(window).height() - 60;
+
+    if (this.next && scrollOffset > threshold) {
+      if (!this.loading) {
+        this.getList();
+      }
+    }
+
+  }
+
 }
