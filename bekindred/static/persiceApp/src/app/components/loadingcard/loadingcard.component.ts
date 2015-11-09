8b9499c1cf25b0440e6b2a8b7cb6a0b13bb59ffd
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-11-09 15:36:13 +0100

    ICE-1223 Added page loading graphic animation

--- bekindred/static/persiceApp/src/app/components/loadingcard/loadingcard.component.ts
+++ bekindred/static/persiceApp/src/app/components/loadingcard/loadingcard.component.ts
@@ -1,13 +1,14 @@
 /// <reference path="../../../typings/_custom.d.ts" />
 
 import {Component, NgIf} from 'angular2/angular2';
+import {CircleProgressDirective} from '../../directives/circleprogress.directive';
 
 let view = require('./loadingcard.html');
 @Component({
   inputs: ['status'],
   selector: 'loading-card',
   template: view,
-  directives: [NgIf]
+  directives: [NgIf, CircleProgressDirective]
 })
 export class LoadingCardComponent {
   status: boolean;
--- bekindred/static/persiceApp/src/app/directives/circleprogress.directive.ts
+++ bekindred/static/persiceApp/src/app/directives/circleprogress.directive.ts
@@ -23,7 +23,7 @@ export class CircleProgressDirective {
   onInit() {
     if (jQuery(this.el.nativeElement).data('circle-progress') === undefined) {
       jQuery(this.el.nativeElement).circleProgress({
-        value: this.calculateValue(this.value),
+        value: this.calculateValue(parseFloat(this.value)),
         size: parseInt(this.size, 10),
         thickness: parseInt(this.thickness, 10),
         startAngle: parseInt(this.angle, 10),
