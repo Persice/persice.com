21d20a85bc1f0ea3ea1d6b10e4dc66c504bade14
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-11-13 17:01:23 +0100

    Renamed components connections and messages

--- bekindred/static/persiceApp/src/app/components/messages/messages.component.ts
+++ bekindred/static/persiceApp/src/app/components/messages/messages.component.ts
@@ -3,13 +3,13 @@
 import {Component} from 'angular2/angular2';
 import {RouterLink } from 'angular2/router';
 
-let view = require('./message.html');
+let view = require('./messages.html');
 @Component({
   selector: 'message',
   template: view,
   directives: [RouterLink]
 })
-export class MessageComponent {
+export class MessagesComponent {
   onInit() {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
