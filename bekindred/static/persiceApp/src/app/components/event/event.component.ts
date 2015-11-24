d32a0ca7d1b832989ecd3645fdb24a9d5e578e8f
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-11-24 14:21:25 +0100

    ICE-1294 Added Event component

---
+++ bekindred/static/persiceApp/src/app/components/event/event.component.ts
@@ -0,0 +1,14 @@
+/// <reference path="../../../typings/_custom.d.ts" />
+
+import {Component} from 'angular2/angular2';
+
+let view = require('./event.html');
+
+@Component({
+  selector: 'event',
+  template: view
+})
+export class EventComponent {
+
+
+}
