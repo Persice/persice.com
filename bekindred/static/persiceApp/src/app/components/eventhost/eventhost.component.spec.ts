6a3cc0a3149fdd97dc35ae3cce3936546b3fae18
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-11-24 17:12:33 +0100

    ICE-1306 Created unit tests for single event components

---
+++ bekindred/static/persiceApp/src/app/components/eventhost/eventhost.component.spec.ts
@@ -0,0 +1,47 @@
+/// <reference path="../../../typings/_custom.d.ts" />
+
+import {
+iit,
+it,
+ddescribe,
+describe,
+expect,
+inject,
+injectAsync,
+TestComponentBuilder,
+beforeEachProviders,
+fakeAsync,
+tick
+} from 'angular2/testing';
+
+import {Component, View, provide} from 'angular2/angular2';
+
+import {EventHostComponent} from './eventhost.component';
+
+// Create a test component to test directives
+@Component({
+  template: '',
+  directives: [EventHostComponent]
+})
+class TestComponent {
+
+}
+
+describe('EventHost component', () => {
+
+
+  it('should exist', injectAsync([TestComponentBuilder], (tcb) => {
+    return tcb.overrideTemplate(TestComponent, '<div><event-host></event-host><div>')
+      .createAsync(TestComponent).then((fixture: any) => {
+        fixture.detectChanges();
+        let componentInstance = fixture.debugElement.componentViewChildren[0].componentInstance;
+        let componentDOMEl = fixture.debugElement.nativeElement;
+        let elRef = fixture.debugElement.elementRef;
+
+        expect(elRef).not.toBeNull(true);
+
+
+      });
+  }));
+
+});
---
+++ bekindred/static/persiceApp/src/app/components/eventphotomap/eventphotomap.component.spec.ts
@@ -0,0 +1,47 @@
+/// <reference path="../../../typings/_custom.d.ts" />
+
+import {
+iit,
+it,
+ddescribe,
+describe,
+expect,
+inject,
+injectAsync,
+TestComponentBuilder,
+beforeEachProviders,
+fakeAsync,
+tick
+} from 'angular2/testing';
+
+import {Component, View, provide} from 'angular2/angular2';
+
+import {EventPhotoMapComponent} from './eventphotomap.component';
+
+// Create a test component to test directives
+@Component({
+  template: '',
+  directives: [EventPhotoMapComponent]
+})
+class TestComponent {
+
+}
+
+describe('EventPhotoMap component', () => {
+
+
+  it('should exist', injectAsync([TestComponentBuilder], (tcb) => {
+    return tcb.overrideTemplate(TestComponent, '<div><event-photomap></event-photomap><div>')
+      .createAsync(TestComponent).then((fixture: any) => {
+        fixture.detectChanges();
+        let componentInstance = fixture.debugElement.componentViewChildren[0].componentInstance;
+        let componentDOMEl = fixture.debugElement.nativeElement;
+        let elRef = fixture.debugElement.elementRef;
+
+        expect(elRef).not.toBeNull(true);
+
+
+      });
+  }));
+
+});
