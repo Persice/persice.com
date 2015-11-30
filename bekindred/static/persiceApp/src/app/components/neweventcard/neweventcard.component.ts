c77e0031929287429a685b42161bbb49cce79cd4
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-11-30 20:50:03 +0100

    ICE-1302 Created remodal component for new event

--- bekindred/static/persiceApp/src/app/components/neweventcard/neweventcard.component.ts
+++ bekindred/static/persiceApp/src/app/components/neweventcard/neweventcard.component.ts
@@ -1,6 +1,9 @@
 /// <reference path="../../../typings/_custom.d.ts" />
 
 import {Component, EventEmitter} from 'angular2/angular2';
+
+declare var jQuery: any;
+
 let view = require('./neweventcard.html');
 
 @Component({
@@ -14,6 +17,19 @@ export class NewEventCardComponent {
 
   userClicked() {
     this.onClick.next(true);
+  }
+
+  saveEvent(event) {
+    console.log('Save event');
+  }
+
+  afterViewInit() {
+    let options = {
+      hashTracking: false,
+      closeOnOutsideClick: false
+    };
+
+    jQuery('[data-remodal-id=create-event]').remodal(options);
   }
 
 }
