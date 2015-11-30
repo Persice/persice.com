13f966b2a67fb0ab916ca98526169e15adafa3b1
Andrii Soldatenko <andrii.soldatenko@datarobot.com>
2015-11-30 20:09:57 +0200

    ICE-1317 User's profile not getting updated when new interests are added

---
+++ bekindred/interests/signals.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from interests.models import Interest
+from interests.tasks import update_index_delay
+
+models.signals.post_save.connect(update_index_delay, sender=Interest)
+models.signals.post_delete.connect(update_index_delay, sender=Interest)
+
+models.signals.post_save.connect(update_index_delay, sender=Interest)
+models.signals.post_delete.connect(update_index_delay, sender=Interest)
