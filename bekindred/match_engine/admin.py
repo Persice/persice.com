c6e439fe69e850b020f67f8f1671e034e5cc7cff
Andrii Soldatenko <andrii.soldatenko@datarobot.com>
2015-12-07 23:00:00 +0200

    ICE-1348 Add way to manage gerund keywords to Admin panel

--- bekindred/match_engine/admin.py
+++ bekindred/match_engine/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from match_engine.models import StopWords
+from match_engine.models import StopWords, GerundWords
 
 
 class StopWordsResource(resources.ModelResource):
@@ -13,4 +13,15 @@ class StopWordsResource(resources.ModelResource):
 class StopWordsAdmin(ImportExportModelAdmin):
     resource_class = StopWordsResource
 
+
+class GerundWordResource(resources.ModelResource):
+    class Meta:
+        model = GerundWords
+
+
+class GerundWordAdmin(ImportExportModelAdmin):
+    resource_class = GerundWordResource
+
+
 admin.site.register(StopWords, StopWordsAdmin)
+admin.site.register(GerundWords, GerundWordAdmin)
