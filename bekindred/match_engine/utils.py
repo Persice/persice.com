6acf885f0e3249540d9f88884b2d151d94a83c8e
Andrii Soldatenko <andrii.soldatenko@datarobot.com>
2015-12-18 15:46:39 +0200

    fix bug with stopwords

--- bekindred/match_engine/utils.py
+++ bekindred/match_engine/utils.py
@@ -8,7 +8,9 @@ from match_engine.models import CollocationDict, StopWords
 def find_collocations(keywords):
     collocations = CollocationDict.objects. \
         all().values_list('phrase', flat=True)
+    stop_words = StopWords.objects.all().values_list('word', flat=True)
     s = PorterStemmer()
+    st_stop_words = [s.stem(w) for w in stop_words]
     keywords_ = keywords
     c = Counter()
     for phrase in collocations:
@@ -17,7 +19,17 @@ def find_collocations(keywords):
             if s.stem(single_phrase[0]) == s.stem(keyword) or \
                s.stem(single_phrase[1]) == s.stem(keyword):
                     c[phrase] += 1
-    d = dict(c)
+    new_d = dict(c)
+    d = dict()
+    for key, value in new_d.iteritems():
+        if value == 2:
+            d[key] = value
+        elif value == 1:
+            ph = key.split()
+            if s.stem(ph[0]) in st_stop_words or \
+               s.stem(ph[1]) in st_stop_words:
+               d[key] = value
+
     result = []
 
     temp = []
