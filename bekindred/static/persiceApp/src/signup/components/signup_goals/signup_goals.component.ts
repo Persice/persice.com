3db8b1037a8306d5b74e67949b8c5fb70ac2b19d
Sasa Macakanja <sasa.macakanja@gmail.com>
2015-12-18 01:39:39 +0100

    ICE-1455 Added Onboarding -- Goals page

--- bekindred/static/persiceApp/src/signup/components/signup_goals/signup_goals.component.ts
+++ bekindred/static/persiceApp/src/signup/components/signup_goals/signup_goals.component.ts
@@ -1,11 +1,248 @@
-import {Component} from 'angular2/core';
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
 
+import {findIndex, pluck} from 'lodash';
+
+import {GoalsService} from '../../../app/services/goals.service';
+import {NotificationService} from '../../../app/services/notification.service';
+
+import {LoadingComponent} from '../../../app/components/loading/loading.component';
 let view = require('./signup_goals.html');
+
+
+declare var jQuery: any;
+declare var Bloodhound: any;
 
 @Component({
   selector: 'signup-goals',
-  template: view
+  template: view,
+  directives: [
+    LoadingComponent
+  ]
 })
 export class SignupGoalsComponent {
+
+  @Output() counter: EventEmitter<any> = new EventEmitter();
+
+  timeoutId = null;
+
+  items: any[] = [];
+  loading: boolean = false;
+  isListEmpty: boolean = false;
+  limit: number = 12;
+  query: string = '';
+  next: string = '';
+  total_count: number = 0;
+  offset: number = 0;
+  newGoal = '';
+
+  constructor(
+    private goalsService: GoalsService,
+    private notificationService: NotificationService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.getList();
+    this.initializeTokenInput();
+  }
+
+  ngOnDestroy() {
+    jQuery('#goalsInput').typeahead('destroy');
+  }
+
+  initializeTokenInput() {
+    let keywordsEngine = new Bloodhound({
+      remote: {
+        url: '/api/v1/subject/?format=json&description__icontains=%QUERY',
+        filter: (x: any) => {
+          return jQuery.map(x.objects, (item) => {
+            return item.description;
+          });
+        },
+        wildcard: '%QUERY'
+      },
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace
+    });
+
+    keywordsEngine.initialize();
+
+
+    jQuery('#goalsInput').typeahead(
+      {
+        hint: false,
+        highlight: true,
+        minLength: 2,
+        limit: 20
+      },
+      {
+        source: keywordsEngine
+      }
+    );
+
+    jQuery('#goalsInput').bind('typeahead:select', (ev, suggestion) => {
+      console.log('Selection: ' + suggestion);
+      this.newGoal = suggestion;
+      // this.addGoal(true);
+    });
+
+
+  }
+
+  addGoal(event) {
+
+    if (this.newGoal.length === 0) {
+      this.notificationService.push({
+        type: 'warning',
+        title: '',
+        body: `Please enter your goal first.`,
+        autoclose: 4000
+      });
+      return;
+    }
+
+    if (this.newGoal.length > 100) {
+      this.notificationService.push({
+        type: 'warning',
+        title: '',
+        body: `New goal must have less than 100 characters.`,
+        autoclose: 4000
+      });
+      return;
+    }
+
+    this.goalsService.save(this.newGoal)
+      .subscribe((res) => {
+        let newItem = res;
+        this.items.push(newItem);
+
+        this.notificationService.push({
+          type: 'success',
+          title: '',
+          body: `Goal '${this.newGoal}' has been successfully created.`,
+          autoclose: 4000
+        });
+
+        this.total_count++;
+        this.counter.next({
+          type: 'goals',
+          count: this.total_count
+        });
+
+        this.newGoal = '';
+        jQuery('#goalsInput').typeahead('val', '');
+
+      }, (err) => {
+        console.log(err);
+        let error = JSON.parse(err._body);
+        if ('goal' in error) {
+          this.notificationService.push({
+            type: 'error',
+            title: '',
+            body: `${error.goal.error[0]}`,
+            autoclose: 4000
+          });
+        }
+      }, () => {
+
+      });
+
+  }
+
+  removeGoal(event) {
+    let idx = findIndex(this.items, event);
+    if (this.items[idx]) {
+      this.goalsService.delete(event.resource_uri)
+        .subscribe((res) => {
+          this.items.splice(idx, 1);
+          this.total_count--;
+          this.counter.next({
+            type: 'goals',
+            count: this.total_count
+          });
+        });
+    }
+
+  }
+
+
+  getList() {
+    if (this.next === null) return;
+    this.loading = true;
+    this.goalsService.get(this.next, 100)
+      .subscribe(data => this.assignList(data),
+      (err) => {
+        console.log(err);
+        this.loading = false;
+      },
+      () => {
+
+      });
+  }
+
+
+  refreshList() {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    this.items.splice(0, this.items.length);
+    this.isListEmpty = false;
+    this.next = '';
+    this.getList();
+  }
+
+  assignList(data) {
+
+    this.loading = false;
+
+    this.total_count = data.meta.total_count;
+
+    this.counter.next({
+      type: 'goals',
+      count: this.total_count
+    });
+
+    if (this.total_count === 0) {
+      this.isListEmpty = true;
+      return;
+    } else {
+      this.isListEmpty = false;
+    }
+
+    if (this.items.length > 0) {
+      let more = data.objects;
+      for (var i = 0; i <= more.length - 1; i++) {
+        this.items.push(more[i]);
+      }
+
+    }
+    else {
+      this.items = data.objects;
+    }
+
+    this.next = data.meta.next;
+    this.offset = data.meta.offset;
+
+
+    //bind to scroll event to load more data on bottom scroll
+    if (this.next !== null) {
+      jQuery('#goals').bind('scroll', this.handleScrollEvent.bind(this));
+    }
+    else {
+      jQuery('#goals').unbind('scroll');
+    }
+
+
+  }
+
+  handleScrollEvent(event) {
+    let scrollOffset = jQuery('#goals').scrollTop() + jQuery('#goals').innerHeight();
+    let threshold = jQuery('#goals')[0].scrollHeight;
+    if (this.next && scrollOffset >= threshold) {
+      if (!this.loading) {
+        this.getList();
+      }
+    }
+
+  }
 
 }
