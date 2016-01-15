fc9c9724d99cb5cf7499ecc945e0078f56b166ff
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-01-15 13:44:17 +0100

    ICE-1383 Added geolocation feature in frontend

--- bekindred/static/persiceApp/src/app/services/services.ts
+++ bekindred/static/persiceApp/src/app/services/services.ts
@@ -24,6 +24,8 @@ import {likesServiceInjectables} from './likes.service';
 import {religiousViewsServiceInjectables} from './religiousviews.service';
 import {politicalViewsServiceInjectables} from './politicalviews.service';
 import {myProfileServiceInjectables} from './myprofile.service';
+import {locationServiceInjectables} from './location.service';
+import {geolocationServiceInjectables} from './geolocation.service';
 
 export * from './user.service';
 export * from './search.service';
@@ -51,6 +53,8 @@ export * from './likes.service';
 export * from './religiousviews.service';
 export * from './politicalviews.service';
 export * from './myprofile.service';
+export * from './location.service';
+export * from './geolocation.service';
 
 export var APP_SERVICES_PROVIDERS: Array<any> = [
   userServiceInjectables,
@@ -77,5 +81,7 @@ export var APP_SERVICES_PROVIDERS: Array<any> = [
   likesServiceInjectables,
   religiousViewsServiceInjectables,
   politicalViewsServiceInjectables,
-  myProfileServiceInjectables
+  myProfileServiceInjectables,
+  locationServiceInjectables,
+  geolocationServiceInjectables
 ];
