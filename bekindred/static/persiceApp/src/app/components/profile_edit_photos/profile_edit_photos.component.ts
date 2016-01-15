37f1ef1cd7b062e27edb563d871943767e336a3d
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-01-15 11:26:52 +0100

    ICE-1408 Added photos component for edit profile and sortable directive

--- bekindred/static/persiceApp/src/app/components/profile_edit_photos/profile_edit_photos.component.ts
+++ bekindred/static/persiceApp/src/app/components/profile_edit_photos/profile_edit_photos.component.ts
@@ -1,11 +1,37 @@
 import {Component, Input, Output, ChangeDetectionStrategy} from 'angular2/core';
 
+
+import {SortableDirective} from '../../directives/sortable.directive';
+
+import {ListUtil} from '../../core/util';
+
 let view = require('./profile_edit_photos.html');
 
 @Component({
-  selector: 'profile-edit-photo',
-  template: view
+  selector: 'profile-edit-photos',
+  template: view,
+  directives: [
+    SortableDirective
+  ]
 })
 export class ProfileEditPhotosComponent {
   @Input() photos;
+  @Input() default;
+  profilePhotos = [];
+
+  ngOnChanges(values) {
+    if (values.photos && values.photos.currentValue) {
+      this.assignPhotos(values.photos.currentValue);
+    }
+  }
+
+  assignPhotos(photos) {
+    this.profilePhotos = ListUtil.orderBy(photos, ['order'], ['asc']);
+    if (this.profilePhotos.length === 1 || this.profilePhotos.length === 0) {
+      this.profilePhotos = [{
+        photo: this.default,
+        order: 0
+      }];
+    }
+  }
 }
