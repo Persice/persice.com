30f17fa6d8635fc2079c30ba1d1beb536b3dcf5a
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-01-21 23:44:09 +0100

    Reverted clear_db.sh

--- clear_db.sh
+++ clear_db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=bekindred.settings.local
 
-export PGPASSWORD='postgres';
-psql -d geodjango -U postgres -f clear_database.sql | grep drop > clear.sql
-psql -d geodjango -U postgres -f clear.sql
+psql -d geodjango -U bekindred -f clear_database.sql | grep drop > clear.sql
+psql -d geodjango -U bekindred -f clear.sql
 rm clear.sql
 
 python bekindred/manage.py syncdb
