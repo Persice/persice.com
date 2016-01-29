45658e73e7b745caadfb0c5f2be4ec7d7e9dedd9
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-01-29 05:25:07 +0100

    ICE-1527 Fixed likes when scrolling users in crowd and connections

--- bekindred/static/persiceApp/src/app/components/profile/base_profile.component.ts
+++ bekindred/static/persiceApp/src/app/components/profile/base_profile.component.ts
@@ -64,13 +64,14 @@ export abstract class BaseProfileComponent {
     public religiousviewsService: ReligiousViewsService,
     public politicalviewsService: PoliticalViewsService,
     public type: string
-    ) {
+  ) {
     this.profileType = type;
   }
 
   assignUser() {
     this.loadingConnections = true;
     this.loadingPhotos = true;
+    this.loadingLikes = true;
 
     this.profileId = this.user.id;
     this.profileName = this.user.first_name;
@@ -79,10 +80,15 @@ export abstract class BaseProfileComponent {
     this.profileDistance = `${this.user.distance[0]} ${this.user.distance[1]}`;
     this.profileLocation = this.user.lives_in ? this.user.lives_in : '';
 
+
     setTimeout(() => {
+      this.profileLikesCount = 0;
+      this.profileLikes = [];
       let likes = this.user.likes;
       this.profileLikes = likes;
       this.profileLikesCount = this.profileLikes.length;
+      this.loadingLikes = false;
+
     });
 
     this.profileJob = this.user.position && this.user.position.job !== null && this.user.position.company !== null ? `${this.user.position.job} at ${this.user.position.company}` : '';
@@ -113,17 +119,17 @@ export abstract class BaseProfileComponent {
 
   getMutualFriends(id) {
     this.mutualfriendsService.get('', 100, id)
-    .subscribe(data => this.assignMutualFriends(data));
+      .subscribe(data => this.assignMutualFriends(data));
   }
 
   getReligiousViews(id) {
     this.religiousviewsService.getByUser('', 100, id)
-    .subscribe(data => this.assignReligiousViews(data));
+      .subscribe(data => this.assignReligiousViews(data));
   }
 
   getPoliticalViews(id) {
     this.politicalviewsService.getByUser('', 100, id)
-    .subscribe(
+      .subscribe(
       data => this.assignPoliticalViews(data),
       (err) => console.log('Error fetching political views'),
       () => { }
@@ -132,7 +138,7 @@ export abstract class BaseProfileComponent {
 
   getPhotos(id) {
     this.photosService.get('', 6, id)
-    .subscribe(data => this.assignPhotos(data));
+      .subscribe(data => this.assignPhotos(data));
   }
 
   assignPhotos(data) {
