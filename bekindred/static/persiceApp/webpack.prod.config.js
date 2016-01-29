ea10404a8770fbbb72b58b3d5523529ebc0fa95f
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-01-29 10:23:03 +0100

    Modified todo reminder in webpack production conf

--- bekindred/static/persiceApp/webpack.prod.config.js
+++ bekindred/static/persiceApp/webpack.prod.config.js
@@ -166,8 +166,8 @@ module.exports = {
       // comments: true,//debug
 
       beautify: false, //prod
-      // disable mangling because of a bug in angular2 beta.1
-      // TODO: enable mangling as soon as angular2 beta.2 is out
+      // disable mangling because of a bug in angular2 beta.2
+      // TODO: enable mangling as soon as angular2 beta.3 is out
       // mangle: {
       //   screw_ie8: true
       // }, //prod
