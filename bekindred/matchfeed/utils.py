6a2a8f5629c1aeec4b472f647382aea584a713ce
Andrii Soldatenko <andrii.soldatenko@datarobot.com>
2016-02-25 08:46:10 +0200

    ICE-1639 Small fix with broken photo links

--- bekindred/matchfeed/utils.py
+++ bekindred/matchfeed/utils.py
@@ -154,7 +154,7 @@ class MatchUser(object):
         self.first_name = self.user.first_name
         self.last_name = self.user.last_name
         self.facebook_id = self.user.facebook_id
-        self.image = self.user.image
+        self.image = self.user.image.url
         self.age = calculate_age(self.user.date_of_birth)
         self.gender = self.user.gender or 'm,f'
         self.about = self.user.about_me
