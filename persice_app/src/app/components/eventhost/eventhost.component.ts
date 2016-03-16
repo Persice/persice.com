5769a589ef452905ed269fc74d7b3a3111bdceaf
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-03-16 16:51:36 +0100

    ICE-1723 Fixed image for event host

--- persice_app/src/app/components/eventhost/eventhost.component.ts
+++ persice_app/src/app/components/eventhost/eventhost.component.ts
@@ -1,11 +1,27 @@
 import {Component, Input} from 'angular2/core';
 import {Router} from 'angular2/router';
 
-let view = require('./eventhost.html');
+import {CheckImageDirective} from '../../directives/checkimage.directive';
 
 @Component({
   selector: 'event-host',
-  template: view
+  template: `
+    <h3 class="module-title">Event host</h3>
+    <div class="flag flag--small mb" id="eventHost" (click)="openProfile(host.username)">
+      <div class="flag__img">
+        <div class="avatar-holder" checkimage="{{host.image}}" [suffix]="'.56x56_q100_crop.jpg'" [onchanges]="1">
+        </div>
+      </div>
+      <div class="flag__body">
+        <h5 class="host-name">{{host.name}}</h5>
+        <p class="single-title-subinfo single-title-subinfo--small">{{host.gender}} / Age {{host.age}} {{host.distance}}</p>
+      </div>
+    </div>
+    <p class="module-type">{{host.description}}</p>
+  `,
+  directives: [
+    CheckImageDirective
+  ]
 })
 export class EventHostComponent {
   @Input() host;
