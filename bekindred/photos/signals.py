6fed15018b3d91ac6b776a85aa53dca7d5308b43
Andrii Soldatenko <andrii.soldatenko@datarobot.com>
2016-03-16 14:58:24 +0200

    ICE-1718 Main profile photo does not update immediately on Crowd/Connections pages

--- bekindred/photos/signals.py
+++ bekindred/photos/signals.py
@@ -1,5 +1,8 @@
 from django.dispatch import receiver
+from django.db import models
 from easy_thumbnails.signals import saved_file
+
+from photos.models import FacebookPhoto
 from tasks import generate_thumbnails
 
 
@@ -8,3 +11,7 @@ def generate_thumbnails_async(sender, fieldfile, **kwargs):
     generate_thumbnails.delay(
         model=sender, pk=fieldfile.instance.pk,
         field=fieldfile.field.name)
+
+
+models.signals.post_save.connect(update_index_delay, sender=FacebookPhoto)
+models.signals.post_delete.connect(update_index_delay, sender=FacebookPhoto)
