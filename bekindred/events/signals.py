d89359d878a44e347706a7129900e1542756dd20
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2016-04-07 23:31:21 +0300

    ICE-1744 User getting Event does not exist error

--- bekindred/events/signals.py
+++ bekindred/events/signals.py
@@ -11,9 +11,10 @@ from guardian.shortcuts import assign_perm
 from events.models import Membership, Event
 from events.tasks import update_index_delay
 from events.tasks import update_match_score
+from friends import Friend
 from interests.models import PoliticalIndex, PoliticalView, ReligiousIndex, \
     ReligiousView
-from members.models import OnBoardingFlow
+from members.models import OnBoardingFlow, FacebookCustomUserActive
 
 signals.post_save.connect(update_match_score, sender=Membership)
 signals.post_delete.connect(update_match_score,
@@ -67,7 +68,24 @@ def add_permissions(sender, **kwargs):
         public_events = Event.objects.filter(access_level='public')
         for event in public_events:
             assign_perm('view_event', user, event)
+
 post_save.connect(add_permissions, sender=FacebookCustomUser)
+
+
+def update_connections_permissions(sender, **kwargs):
+    friends = kwargs.get("instance")
+    if friends.status == 1:
+        events1 = Membership.objects.filter(user=friends.friend1,
+                                            is_organizer=True)
+        for event1 in events1:
+            assign_perm('view_event', friends.friend2, event1.event)
+
+        events2 = Membership.objects.filter(user=friends.friend2,
+                                            is_organizer=True)
+        for event2 in events2:
+            assign_perm('view_event', friends.friend1, event2.event)
+
+post_save.connect(update_connections_permissions, sender=Friend)
 
 models.signals.post_save.connect(update_index_delay, sender=Event)
 models.signals.post_delete.connect(update_index_delay, sender=Event)
