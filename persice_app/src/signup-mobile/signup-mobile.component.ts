b85ebc69b4a342a66733f64925bd85f663857ef1
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-04-12 18:30:45 +0200

    ICE-1778 Added selector for mobile signup app: persice-signup-mobile-app

--- persice_app/src/signup-mobile/signup-mobile.component.ts
+++ persice_app/src/signup-mobile/signup-mobile.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'prs-signup-mobile',
+  selector: 'persice-signup-mobile-app',
   template: require('./signup-mobile.html')
 })
 export class SignupMobileComponent {
