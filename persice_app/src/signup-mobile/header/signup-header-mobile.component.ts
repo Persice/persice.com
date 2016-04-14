109ce7c7f5c132a8f206a0f1f07754364c3cf291
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-04-14 12:15:49 +0200

    ICE-1781 Finished header component for signup mobile

--- persice_app/src/signup-mobile/header/signup-header-mobile.component.ts
+++ persice_app/src/signup-mobile/header/signup-header-mobile.component.ts
@@ -1,9 +1,19 @@
-import {Component} from 'angular2/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter
+} from 'angular2/core';
 
 @Component({
   selector: 'prs-mobile-signup-header',
   template: require('./signup-header-mobile.html')
 })
 export class SignupHeaderMobileComponent {
-
+  @Input() counter;
+  @Input() nextTitle;
+  @Input() title;
+  @Input() showBack;
+  @Output() next: EventEmitter<any> = new EventEmitter();
+  @Output() back: EventEmitter<any> = new EventEmitter();
 }
