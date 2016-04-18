ccf788fa29771a6ec9ee025235e421aae000af6c
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-04-18 14:38:35 +0200

    feat(webpack.config): switch env

--- persice_app/webpack.config.js
+++ persice_app/webpack.config.js
@@ -1,3 +1,14 @@
 // Persice
 // Look in ./config folder for webpack.dev.js
-module.exports = require('./config/webpack.dev.js');
+switch (process.env.NODE_ENV) {
+  case 'production':
+    module.exports = require('./config/webpack.prod');
+    break;
+  case 'testing':
+    module.exports = require('./config/webpack.test');
+    break;
+  case 'development':
+    module.exports = require('./config/webpack.dev');
+  default:
+    module.exports = require('./config/webpack.dev');
+}
