9452c154e228e728e51d6b3826df50d8e5492fb9
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-04-25 07:05:35 +0200

    chore(karma.conf.js): add mocha reporter

--- persice_app/config/karma.conf.js
+++ persice_app/config/karma.conf.js
@@ -106,7 +106,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'coverage'],
+    reporters: ['mocha', 'coverage'],
 
     junitReporter: {
       outputDir: 'test_results/',
