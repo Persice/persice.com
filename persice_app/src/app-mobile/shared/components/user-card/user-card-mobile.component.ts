541ee5e10f36d351bc52709b2c8c880e6363abf2
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-04-27 22:27:31 +0200

    ICE-1826: Users can preview each other's top/shared interests on the Crowd pages (responsive)

--- persice_app/src/app-mobile/shared/components/user-card/user-card-mobile.component.ts
+++ persice_app/src/app-mobile/shared/components/user-card/user-card-mobile.component.ts
@@ -1,11 +1,34 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, ChangeDetectionStrategy} from 'angular2/core';
 import {CheckImageDirective} from "../../../../app/shared/directives";
-
+import {GenderPipe} from '../../../../app/shared/pipes';
+import {ObjectUtil} from '../../../../app/shared/core';
 @Component({
   selector: 'prs-mobile-user-card',
   template: require('./user-card-mobile.html'),
-  directives: [CheckImageDirective]
+  directives: [CheckImageDirective],
+  pipes: [GenderPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserCardMobileComponent {
-  @Input() person;
+  @Input() set person (value: any) {
+    this._person = value;
+    let topInterests = ObjectUtil.firstSorted(value.top_interests[0], 6);
+    let halfLength = Math.ceil(topInterests.length / 2);
+    this.interestsLeftHalf = topInterests.splice(0, halfLength);
+    this.interestsRightHalf = topInterests;
+  };
+
+  // Person object which is displayed in the component template
+  _person: any;
+
+  // Collection of top interests split in two groups of three
+  interestsLeftHalf: any[] = [];
+  interestsRightHalf: any[] = [];
+
+  //Whether top interests drop is visible.
+  interestsVisible: boolean = false;
+
+  toggleInterestsVisible(event) {
+    this.interestsVisible = !this.interestsVisible;
+  }
 }
