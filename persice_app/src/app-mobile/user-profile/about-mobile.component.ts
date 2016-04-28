d2ffaca3194e0d2955ef8aceb9b683e1791ebdb9
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-04-28 07:34:18 +0200

    ICE-1840 Created component for responsive about me in user profile

---
+++ persice_app/src/app-mobile/user-profile/about-mobile.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, ChangeDetectionStrategy } from 'angular2/core';
+
+const SHOW_CHAR_LIMIT: number = 80;
+
+@Component({
+  selector: 'prs-mobile-profile-about-me',
+  template: require('./about-mobile.html'),
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AboutMobileComponent {
+  @Input() set about(value: string) {
+    this._setValue(value);
+  }
+
+  // About me less and more contents
+  aboutLess: string = '';
+  aboutMore: string = '';
+
+  // Flag for showing more about me
+  showMore: boolean = false;
+
+  constructor() {}
+
+  public toggleMore(value) {
+    this.showMore = value;
+  }
+
+  private _setValue(value: string) {
+    let contents = value !== null ? value : '';
+    if (contents.length > SHOW_CHAR_LIMIT) {
+      this.aboutLess = contents.substring(0, SHOW_CHAR_LIMIT);
+      this.aboutMore = contents.substring(SHOW_CHAR_LIMIT, contents.length).trim();
+    } else {
+      this.aboutLess = contents;
+    }
+  }
+}
