f332ea7fea8b442e05a4cd09940cfaa320374dbe
Ljubomir Simin <ljubomir@coingcs.com>
2016-05-05 14:17:31 +0200

    ICE-1837 Refactoring and test coverage for displaying interests on mobile.

---
+++ persice_app/src/app-mobile/shared/model/person.spec.ts
@@ -0,0 +1,41 @@
+import {PersonGenerator} from "./person-generator";
+import {Person} from "./person";
+
+describe('Person', () => {
+
+  it('instantiates from dto', () => {
+    // given
+    let dto: any = PersonGenerator.givenAnyPersonDto();
+
+    // when
+    let person: Person = new Person(dto);
+
+    // then
+    expect(person.firstName).toEqual(dto.first_name);
+    expect(person.lastName).toEqual(dto.last_name);
+    expect(person.gender).toEqual(dto.gender === 'm' ? 'male' : 'female');
+    expect(person.score).toEqual(dto.score);
+    expect(person.age).toEqual(dto.age);
+    expect(person.distance).toEqual(dto.distance[0]);
+    expect(person.distanceUnit).toEqual(dto.distance[1]);
+    expect(person.topInterests.length).toBeGreaterThan(0);
+    expect(person.image).toEqual(dto.image);
+  });
+
+  it('determines first and second half of top interests', () => {
+    // given
+    let person: Person = PersonGenerator.givenAnyPerson();
+
+    // then
+    expect(person.topInterests.length)
+      .toEqual(person.topInterestsFirstHalf.length + person.topInterestsSecondHalf.length);
+
+    for (let interest of person.topInterestsFirstHalf) {
+      expect(person.topInterests).toContain(interest);
+    }
+
+    for (let interest of person.topInterestsSecondHalf) {
+      expect(person.topInterests).toContain(interest);
+    }
+  });
+});
---
+++ persice_app/src/app-mobile/shared/model/person.ts
@@ -0,0 +1,76 @@
+import {ObjectUtil} from "../../../app/shared/core/util";
+export class Person {
+
+  public topInterestsFirstHalf: any[];
+  public topInterestsSecondHalf: any[];
+
+  private _id: string;
+  private _firstName: string;
+  private _lastName: string;
+  private _gender: string;
+  private _age: number;
+  private _distance: number;
+  private _distanceUnit: string;
+  private _topInterests: any[];
+  private _image: string;
+  private _score: number;
+
+  constructor(dto: any) {
+    this._id = dto._id;
+    this._firstName = dto.first_name;
+    this._lastName = dto.last_name;
+    this._gender = dto.gender === 'm' ? 'male' : 'female';
+    this._age = dto.age;
+    this._distance = dto.distance[0];
+    this._distanceUnit = dto.distance[1];
+    this._image = dto.image;
+    this._score = dto.score;
+
+    let interestsFromDto = ObjectUtil.firstSorted(dto.top_interests[0], 6);
+    let halfLength = Math.ceil(interestsFromDto.length / 2);
+
+    this._topInterests = ObjectUtil.firstSorted(dto.top_interests[0], 6);
+    this.topInterestsFirstHalf = interestsFromDto.splice(0, halfLength);
+    this.topInterestsSecondHalf = interestsFromDto;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get gender(): string {
+    return this._gender;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  get distance(): number {
+    return this._distance;
+  }
+
+  get distanceUnit(): string {
+    return this._distanceUnit;
+  }
+
+  get topInterests(): any[] {
+    return this._topInterests;
+  }
+
+  get image(): string {
+    return this._image;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+}
