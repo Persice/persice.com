d7ca4fcb42f27f097ec28c2df50d40e71e8789bc
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-05-11 23:49:38 +0200

    Fixed unit test for app.component

--- persice_app/src/app/app.component.spec.ts
+++ persice_app/src/app/app.component.spec.ts
@@ -1,83 +1,64 @@
-// import {Component, OnInit, provide} from '@angular/core';
+import {provide} from '@angular/core';
 
-// import {
-//   ROUTER_PRIMARY_COMPONENT,
-//   Router,
-//   RouteRegistry
-// } from '@angular/router-deprecated';
-// import {
-//   it,
-//   describe,
-//   beforeEach,
-//   expect,
-//   inject,
-//   beforeEachProviders
-// } from '@angular/core/testing';
-// import {SpyLocation} from '@angular/common/testing';
-// import {RootRouter} from '@angular/router-deprecated/src/router';
-// import {Location} from '@angular/common';
+import {
+  ROUTER_PRIMARY_COMPONENT,
+  Router,
+  RouteRegistry
+} from '@angular/router-deprecated';
+import {
+  it,
+  describe,
+  beforeEach,
+  expect,
+  inject,
+  beforeEachProviders
+} from '@angular/core/testing';
+import {SpyLocation} from '@angular/common/testing';
+import {RootRouter} from '@angular/router-deprecated/src/router';
+import {Location} from '@angular/common';
 
-// import {AppComponent} from "./app.component";
+import {AppComponent} from "./app.component";
 
-// @Component({
-//   selector: 'prs-test-cmp',
-//   template: '<div>Original Child</div>'
-// })
-// class ChildCmp implements OnInit {
-//   ngOnInit() {
-//     console.log('hello Home component');
-//   }
-// }
+describe('App component', () => {
+  var location, router;
 
-// describe('App component', () => {
-//   var location, router;
+  beforeEachProviders(() => [
+    RouteRegistry,
+    provide(Location, { useClass: SpyLocation }),
+    provide(Router, { useClass: RootRouter }),
+    provide(ROUTER_PRIMARY_COMPONENT, { useValue: AppComponent })
+  ]);
 
-//   beforeEachProviders(() => [
-//     ChildCmp,
-//     RouteRegistry,
-//     provide(Location, { useClass: SpyLocation }),
-//     provide(Router, { useClass: RootRouter }),
-//     provide(ROUTER_PRIMARY_COMPONENT, { useValue: AppComponent })
-//   ]);
+  beforeEach(inject([Router, Location], (r, l) => {
+    router = r;
+    location = l;
+  }));
 
-//   beforeEach(inject([Router, Location], (r, l) => {
-//     router = r;
-//     location = l;
-//   }));
+  it('Should be able to navigate to Crowd', done => {
+    router.navigate(['Crowd']).then(() => {
+      expect(location.path()).toBe('/crowd');
+      done();
+    }).catch(e => done.fail(e));
+  });
 
-//   it('should log ngOnInit', inject([ChildCmp], (app) => {
-//     spyOn(console, 'log');
-//     expect(console.log).not.toHaveBeenCalled();
+  it('Should be able to navigate to Connections', done => {
+    router.navigate(['Connections']).then(() => {
+      expect(location.path()).toBe('/connections');
+      done();
+    }).catch(e => done.fail(e));
+  });
 
-//     app.ngOnInit();
-//     expect(console.log).toHaveBeenCalled();
-//   }));
+  it('Should be able to navigate to Events', done => {
+    router.navigateByUrl('/events').then(() => {
+      expect(location.path()).toBe('/events');
+      done();
+    }).catch(e => done.fail(e));
+  });
 
-//   it('Should be able to navigate to Crowd', done => {
-//     router.navigate(['Crowd']).then(() => {
-//       expect(location.path()).toBe('/crowd');
-//       done();
-//     }).catch(e => done.fail(e));
-//   });
-
-//   it('Should be able to navigate to Connections', done => {
-//     router.navigate(['Connections']).then(() => {
-//       expect(location.path()).toBe('/connections');
-//       done();
-//     }).catch(e => done.fail(e));
-//   });
-
-//   it('Should be able to navigate to Events', done => {
-//     router.navigateByUrl('/events').then(() => {
-//       expect(location.path()).toBe('/events');
-//       done();
-//     }).catch(e => done.fail(e));
-//   });
-
-//   it('Should be able to navigate to Messages', done => {
-//     router.navigate(['Messages']).then(() => {
-//       expect(location.path()).toBe('/messages/new/');
-//       done();
-//     }).catch(e => done.fail(e));
-//   });
-// });
+  it('Should be able to navigate to Messages', done => {
+    router.navigate(['Messages']).then(() => {
+      expect(location.path()).toBe('/messages/new/');
+      done();
+    }).catch(e => done.fail(e));
+  });
+});
