6738911aeea4389034bbafde6ffca9365b1b2404
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-05-12 21:58:28 +0200

    ICE-1917 Added aditional tests and providers

--- persice_app/src/app-mobile/my-profile/my-profile-mobile.component.spec.ts
+++ persice_app/src/app-mobile/my-profile/my-profile-mobile.component.spec.ts
@@ -1,14 +1,18 @@
 import {expect, it, describe, async, inject, beforeEach, beforeEachProviders}
-  from '@angular/core/testing';
+from '@angular/core/testing';
 
 import {TestComponentBuilder} from '@angular/compiler/testing';
 import {RouteParams} from '@angular/router-deprecated';
+import {BaseRequestOptions, Http } from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
 
 import {provide, Provider} from '@angular/core';
 import {Observable} from 'rxjs';
 import {MyProfileMobileComponent} from "./my-profile-mobile.component";
 import {ProfileService} from "../../app/shared/services/profile.service";
+import {AppStateService} from '../shared/services';
 import {PersonGenerator} from "../shared/model/person-generator";
+import {HttpClient} from '../../app/shared/core';
 
 let component: MyProfileMobileComponent;
 let mock: ProfileServiceMock;
@@ -16,7 +20,7 @@ let mock: ProfileServiceMock;
 class ProfileServiceMock extends ProfileService {
   profileDto: any;
 
-  public ofUsername(username:string): Observable<any> {
+  public ofUsername(username: string): Observable<any> {
     return Observable.of(this.profileDto);
   }
 
@@ -36,15 +40,23 @@ describe('My profile mobile component', () => {
     mock = new ProfileServiceMock(null);
     return [
       mock.getProvider(),
+      MockBackend,
+      BaseRequestOptions,
+      HttpClient,
+      provide(Http, {
+        useFactory: (backend, options) => new Http(backend, options),
+        deps: [MockBackend, BaseRequestOptions]
+      }),
+      AppStateService,
       provide(RouteParams,
-        { useValue: new RouteParams({ username: PersonGenerator.givenAnyFirstName()}) })
+        { useValue: new RouteParams({ username: PersonGenerator.givenAnyFirstName() }) })
     ];
   });
 
   beforeEach(async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
     return tcb
       .overrideProviders(
-        MyProfileMobileComponent, [provide(ProfileService, { useValue: mock })])
+      MyProfileMobileComponent, [provide(ProfileService, { useValue: mock })])
       .createAsync(MyProfileMobileComponent)
       .then((componentFixture: any) => {
         fixture = componentFixture;
@@ -52,20 +64,89 @@ describe('My profile mobile component', () => {
       });
   })));
 
-  it('should show username in title', () => {
+  it('should show first name in title', () => {
     // given
     let aPersonDto: any = PersonGenerator.givenAnyPersonDto();
     mock.setResponse(aPersonDto);
 
     // when
-    component.ngOnInit();
+    fixture.detectChanges();
 
     // then
     expect(displayedUsername()).toBe(aPersonDto.first_name);
   });
 
+  it('should show distance value and unit in subtitle', () => {
+    // given
+    let aPersonDto: any = PersonGenerator.givenAnyPersonDto();
+    mock.setResponse(aPersonDto);
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    let expectedDistance = aPersonDto.distance[0] + ' ' + aPersonDto.distance[1] + ' from you';
+    expect(displayedDistance()).toBe(expectedDistance);
+  });
+
+  it('should show gender inside profile box', () => {
+    // given
+    let aPersonDto: any = PersonGenerator.givenAnyPersonDto();
+    mock.setResponse(aPersonDto);
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    expect(displayedGender()).toBe('Female');
+  });
+
+  it('should show age inside profile box', () => {
+    // given
+    let aPersonDto: any = PersonGenerator.givenAnyPersonDto();
+    mock.setResponse(aPersonDto);
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    expect(displayedAge()).toBe(aPersonDto.age);
+  });
+
+  it('should show lives in inside profile box', () => {
+    // given
+    let aPersonDto: any = PersonGenerator.givenAnyPersonDto();
+    mock.setResponse(aPersonDto);
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    expect(displayedLivesIn()).toBe(aPersonDto.lives_in);
+  });
+
   function displayedUsername() {
-    let titleElement = fixture.nativeElement.querySelectorAll('.profile-hero__name');
+    let titleElement = fixture.nativeElement.querySelectorAll('.profile-hero__name')[0];
     return titleElement.textContent;
+  }
+
+  function displayedDistance() {
+    let distanceElement = fixture.nativeElement.querySelectorAll('.profile-hero__name-meta')[0];
+    return distanceElement.textContent;
+  }
+
+  function displayedGender() {
+    let genderElement = fixture.nativeElement.querySelectorAll('.profile-info__gender-value')[0];
+    return genderElement.textContent;
+  }
+
+  function displayedAge() {
+    let ageElement = fixture.nativeElement.querySelectorAll('.profile-info__age-value')[0];
+    return ageElement.textContent;
+  }
+
+  function displayedLivesIn() {
+    let livesinElement = fixture.nativeElement.querySelectorAll('.profile-info__livesin-value')[0];
+    return livesinElement.textContent;
   }
 });
