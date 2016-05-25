c81ffd69736529cfbb8070c38c627ebc7761c0b8
Ljubomir Simin <ljubomir@coingcs.com>
2016-05-25 13:42:05 +0200

    ICE-1932 Edit profile offers - responsive.

---
+++ persice_app/src/app-mobile/edit-my-profile/edit-offers/edit-offers-mobile.component.spec.ts
@@ -0,0 +1,160 @@
+import {expect, it, describe, async, inject, beforeEach, beforeEachProviders}
+from '@angular/core/testing';
+
+import {TestComponentBuilder} from '@angular/compiler/testing';
+
+import {provide, Provider} from '@angular/core';
+import {Observable} from 'rxjs';
+import {EditOffersMobileComponent} from './edit-offers-mobile.component';
+import {OffersService} from '../../../app/shared/services/offers.service';
+import {AppStateService} from '../../shared/services/app-state.service';
+import {OffersGenerators} from './offers-generators';
+
+let component: EditOffersMobileComponent;
+let mock: OffersServiceMock;
+
+class OffersServiceMock extends OffersService {
+  response: any;
+
+  public get(url: string, limit: number): Observable<any> {
+    return Observable.of(this.response);
+  }
+
+  public getCount(): Observable<any> {
+    return this.response.objects.length;
+  }
+
+  public save(subject: string): Observable<any> {
+    return Observable.of(OffersGenerators.givenAnOfferWithSubjectDto(subject));
+  }
+
+  public delete(url: string): Observable<any> {
+    return Observable.of(null);
+  }
+
+  public setEmptyFindResponse(): void {
+    this.response = this.response = {
+      meta: {
+        total_count: 0
+      },
+      objects: []
+    };
+  }
+
+  public setNonEmptyFindResponse(): void {
+    this.response = {
+      meta: {
+        total_count: 2
+      },
+      objects: [
+        OffersGenerators.givenAnOfferWithSubjectDto('find a running partner'),
+        OffersGenerators.givenAnOfferWithSubjectDto('learn to play guitar')
+      ]
+    };
+  }
+
+  public getProvider(): Provider {
+    return provide(OffersService, { useValue: this });
+  }
+}
+
+describe('Edit offers mobile component', () => {
+
+  beforeEachProviders(() => {
+    mock = new OffersServiceMock(null);
+    return [
+      mock.getProvider(),
+      AppStateService
+    ];
+  });
+
+  beforeEach(async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+    return tcb
+      .overrideProviders(
+      EditOffersMobileComponent, [provide(OffersService, { useValue: mock })])
+      .createAsync(EditOffersMobileComponent)
+      .then((componentFixture: any) => {
+        component = componentFixture.componentInstance;
+      });
+  })));
+
+  it('should initially be empty', () => {
+    // when
+    mock.setEmptyFindResponse();
+
+    // then
+    expect(component.items.length).toEqual(0);
+  });
+
+  it('should show items when service returns data', () => {
+    // given
+    mock.setNonEmptyFindResponse();
+
+    // when
+    component.ngOnInit();
+
+    // then
+    expect(component.items.length).toEqual(2);
+    expect(component.items[0]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('find a running partner'));
+    expect(component.items[1]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('learn to play guitar'));
+  });
+
+  it('adds items', () => {
+    // given
+    mock.setEmptyFindResponse();
+
+    // when
+    component.ngOnInit();
+    component.newItemText = 'something new';
+    component.add();
+
+    // then
+    expect(component.items[0].id).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('something new').id);
+    expect(component.items[0].resource_uri)
+      .toEqual(OffersGenerators.givenAnOfferWithSubjectDto('something new').resource_uri);
+    expect(component.items[0].subject).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('something new').subject);
+  });
+
+  it('removes items', () => {
+    // given
+    mock.setNonEmptyFindResponse();
+
+    // when
+    component.ngOnInit();
+    component.remove(OffersGenerators.givenAnOfferWithSubjectDto('find a running partner'));
+
+    // then
+    expect(component.items.length).toEqual(1);
+    expect(component.items[0]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('learn to play guitar'));
+  });
+
+  it('fails to add duplicates', () => {
+    // given
+    mock.setNonEmptyFindResponse();
+
+    // when
+    component.ngOnInit();
+    component.newItemText = OffersGenerators.givenAnOfferWithSubjectDto('find a running partner');
+    component.add();
+
+    // then
+    expect(component.items.length).toEqual(2);
+    expect(component.items[0]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('find a running partner'));
+    expect(component.items[1]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('learn to play guitar'));
+  });
+
+  it('fails to add short items', () => {
+    // given
+    mock.setNonEmptyFindResponse();
+
+    // when
+    component.ngOnInit();
+    component.newItemText = 'a';
+    component.add();
+
+    // then
+    expect(component.items.length).toEqual(2);
+    expect(component.items[0]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('find a running partner'));
+    expect(component.items[1]).toEqual(OffersGenerators.givenAnOfferWithSubjectDto('learn to play guitar'));
+  });
+});
---
+++ persice_app/src/app-mobile/edit-my-profile/edit-offers/edit-offers-mobile.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {ManageGoalsOffersComponent} from '../../../common/manage-goals-offers/manage-goals-offers.component';
+import {InfiniteScrollElementDirective} from '../../../common/directives/infinite-scroll-element.directive';
+import {AutocompleteDirective} from '../../../common/directives/autocomplete.directive';
+import {LoadingComponent} from '../../../app/shared/components/loading/loading.component';
+import {OffersService} from '../../../app/shared/services/offers.service';
+import {AppStateService} from '../../shared/services/app-state.service';
+
+@Component({
+  selector: 'prs-mobile-offers',
+  template: require('./offers-mobile.html'),
+  providers: [OffersService],
+  directives: [
+    LoadingComponent,
+    AutocompleteDirective,
+    InfiniteScrollElementDirective
+  ]
+})
+export class EditOffersMobileComponent extends ManageGoalsOffersComponent implements OnInit {
+
+  constructor(
+    protected offersService: OffersService,
+    protected appStateService: AppStateService
+  ) {
+    super(offersService);
+  }
+
+  ngOnInit() {
+    this.appStateService.setEditMyProfileState(
+        { title: 'offers', isDoneButtonVisible: true });
+    this.getList();
+  }
+}
