7cd2874d3ecd134f637698d5f185cb88e821844a
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-05-27 12:10:15 +0300

    automatic commit before deploy fronted

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -e
+set -xe
 
 source .env
 
 pushd persice_app
-
+npm install
 npm run build:prod
-
 popd
 
 git ls-files . | grep -E '(\.js$)|(\.css$)|(\.gz$)|(\.map$)|(\.svg$)' | xargs git add
@@ -16,4 +15,5 @@ git commit -am 'automatic commit before deploy fronted' || true
 
 fab deploy:$1
 
-git diff --name-only HEAD~$2 HEAD | grep static |sed -e "s/bekindred\/static//g" | python invalidate_cache_cloud_front.py
+
+git diff --name-only HEAD~$2 HEAD | grep static | sed -e "s/bekindred\/static//g" | python invalidate_cache_cloud_front.py
