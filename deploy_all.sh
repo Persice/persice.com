609a4f75fd18bda56cf9b554f5b4b05d68b0a81e
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-05-27 12:15:37 +0300

    Finish with deploy script

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -xe
 
-source .env
+# source .env
 
-pushd persice_app
-npm install
-npm run build:prod
-popd
+# pushd persice_app
+# npm install
+# npm run build:prod
+# popd
 
-git ls-files . | grep -E '(\.js$)|(\.css$)|(\.gz$)|(\.map$)|(\.svg$)' | xargs git add
-git add .
+# git ls-files . | grep -E '(\.js$)|(\.css$)|(\.gz$)|(\.map$)|(\.svg$)' | xargs git add
+# git add .
 
-git commit -am 'automatic commit before deploy fronted' || true
+# git commit -am 'automatic commit before deploy fronted' || true
 
-fab deploy:$1
+# fab deploy:$1
 
 
 git diff --name-only HEAD~$2 HEAD | grep static | sed -e "s/bekindred\/static//g" | python invalidate_cache_cloud_front.py
--- invalidate_cache_cloud_front.py
+++ invalidate_cache_cloud_front.py
@@ -6,6 +6,10 @@ import sys
 
 paths = list(map(str.strip, sys.stdin.readlines()))
 print paths
+if not paths:
+    print "There is no changes in frontend"
+    print 'For more detail run git diff --name-only HEAD~$2 HEAD | grep static | sed -e "s/bekindred\/static//g"'
+    exit(0)
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
