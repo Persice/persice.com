28fd0942007d59d14d55985a7dd19ba175a71ca3
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-06-01 09:26:17 +0200

    ICE-2026 Add frontend route for responsive conversations

--- persice_app/src/app-mobile/messages/messages-mobile.component.ts
+++ persice_app/src/app-mobile/messages/messages-mobile.component.ts
@@ -1,9 +1,20 @@
 import {Component} from '@angular/core';
+import {RouteConfig, RouterOutlet} from '@angular/router-deprecated';
+import {ConversationsMobileComponent} from './conversations';
 
 @Component({
   selector: 'prs-mobile-messages',
-  template: require('./messages-mobile.html')
+  template: require('./messages-mobile.html'),
+  directives: [RouterOutlet]
 })
+@RouteConfig([
+  {
+    path: '/',
+    component: ConversationsMobileComponent,
+    name: 'Conversations',
+    useAsDefault: true
+  }
+])
 export class MessagesMobileComponent {
 
 }
