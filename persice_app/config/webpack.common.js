96ff88d80eece02d4a2f69598ee19de5fe409819
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-06-01 09:23:14 +0200

    ICE-1685 Utilize Flux (Redux) architecture for better application state management

--- persice_app/config/webpack.common.js
+++ persice_app/config/webpack.common.js
@@ -103,7 +103,8 @@ module.exports = {
         exclude: [
           // these packages have problems with their sourcemaps
           helpers.root('node_modules/rxjs'),
-          helpers.root('node_modules/@angular')
+          helpers.root('node_modules/@angular'),
+          helpers.root('node_modules/@ngrx')
         ]
       }
 
