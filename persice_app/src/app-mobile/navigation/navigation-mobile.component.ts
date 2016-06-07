7550d8c0c3e4358a24275822647e005660e17a6b
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-06-07 07:12:30 +0200

    ICE-1872 Display unread counters for inbox

--- persice_app/src/app-mobile/navigation/navigation-mobile.component.ts
+++ persice_app/src/app-mobile/navigation/navigation-mobile.component.ts
@@ -1,15 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {RouterLink} from '@angular/router-deprecated';
-import {CookieUtil} from "../../app/shared/core/util";
+
 @Component({
   selector: 'prs-mobile-navigation',
   template: require('./navigation-mobile.html'),
   directives: [RouterLink]
 })
 export class NavigationMobileComponent {
-  public username: string;
+  @Input() username: string;
+  @Input() unreadMessagesCounter: number;
 
-  constructor() {
-    this.username = CookieUtil.getValue('user_username');
-  }
 }
