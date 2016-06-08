bf6f0d5a7fe2a9104a146e23fbaa2cf2bba2deb1
Ljubomir Simin <ljubomir@coingcs.com>
2016-06-08 17:04:28 +0200

    ICE-1877 User can select a message recipient from a dropdown.

--- persice_app/src/app-mobile/messages/new-conversation/new-conversation-mobile.component.ts
+++ persice_app/src/app-mobile/messages/new-conversation/new-conversation-mobile.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Location} from '@angular/common';
+import {Location, Control} from '@angular/common';
 import {Router, RouteParams} from '@angular/router-deprecated';
-import {Subscription} from 'rxjs';
+import {Subscription, Observable} from 'rxjs';
 
 import {AppStateService} from "../../shared/services/app-state.service";
 import {MessagesService} from "../../../app/shared/services/messages.service";
 import {InboxService} from "../../../app/shared/services/inbox.service";
 import {UserAuthService} from "../../../app/shared/services/userauth.service";
+import {Http} from "@angular/http";
 
 @Component({
   template: require('./new-conversation-mobile.html'),
@@ -20,6 +21,9 @@ export class NewConversationMobileComponent implements OnInit, OnDestroy {
   tokens: any[] = [];
   recipientId: string;
   messageText: string = '';
+  recipientName: string;
+  recipientNameInput = new Control();
+  possibleRecipients: any[];
 
   constructor(
     private appStateService: AppStateService,
@@ -28,11 +32,18 @@ export class NewConversationMobileComponent implements OnInit, OnDestroy {
     private messagesService: MessagesService,
     private userService: UserAuthService,
     private _router: Router,
-    private _params: RouteParams
+    private _params: RouteParams,
+    public http: Http
   ) {
+    // Get recipient from URL, if any.
     this.recipientId = this._params.get('recipientId');
 
-    // Preselect a recipient.
+    // Setup debouncer on recipient input field.
+    this.recipientNameInput.valueChanges
+      .debounceTime(400)
+      .subscribe(() => this.searchRecipientsByPartialName());
+
+    // Preselect a recipient, if any.
     if (this.recipientId !== null) {
       let uri = `/api/v1/auth/user/${this.recipientId}/`;
       let channel = this.userService.findOneByUri(uri)
@@ -59,6 +70,7 @@ export class NewConversationMobileComponent implements OnInit, OnDestroy {
     this._location.back();
   }
 
+  // Send a message.
   send() {
     if (this.tokens.length === 1 && this.messageText.length > 0) {
       let subs: Subscription = this.messagesService.sendNew(this.tokens[0].friend_id, this.messageText)
@@ -68,5 +80,36 @@ export class NewConversationMobileComponent implements OnInit, OnDestroy {
           this._router.parent.navigate(['/Messages', 'Conversation', {senderId: this.tokens[0].friend_id}]);
         }, error => console.log('Could not create a new message.'));
     }
+  }
+
+  // Search for a connection with a given name.
+  public searchRecipientsByPartialName() {
+    this._rawSearch(this.recipientName).subscribe(data => {
+      this.possibleRecipients = data.objects;
+    });
+  }
+
+  // Select a recipient, clear input fields.
+  private selectRecipient(recipient: any) {
+    this.tokens = [{
+      first_name: recipient.first_name,
+      image: recipient.image,
+      friend_id: recipient.friend_id
+    }];
+    this.possibleRecipients = [];
+    this.recipientName = '';
+  }
+
+  // Remove a recipient with a given index.
+  private removeRecipient(i: number) {
+    this.tokens.splice(i, 1);
+  }
+
+  // Perform a connection to backend and retrieve possible recipients.
+  private _rawSearch(term: string): Observable<any> {
+    let url = `/api/v1/connectionssearch/?format=json&first_name=${term}`;
+    return this.http
+      .get(url)
+      .map(response => response.json());
   }
 }
