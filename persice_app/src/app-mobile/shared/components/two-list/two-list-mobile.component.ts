9bac53568b7fc323b84d164919f9191773605fc0
Ljubomir Simin <ljubomir@coingcs.com>
2016-06-16 13:37:38 +0200

    ICE-2079 Groundwork for lazyloading likes.

--- persice_app/src/app-mobile/shared/components/two-list/two-list-mobile.component.ts
+++ persice_app/src/app-mobile/shared/components/two-list/two-list-mobile.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit, OnDestroy} from '@angular/core'
+import {OnInit, OnDestroy} from '@angular/core'
 import {TwoListService} from "../../services/two-list.service";
+
+const STEP_SIZE: number = 20;
 
 export class TwoListMobileComponent implements OnInit, OnDestroy {
 
@@ -13,23 +15,45 @@ export class TwoListMobileComponent implements OnInit, OnDestroy {
   secondListTitle: string;
   secondList: any[] = [];
 
+  firstListNextUrl: string;
+  secondListNextUrl: string;
+
   constructor(
     protected twoListService: TwoListService) {
   }
 
   public ngOnInit(): any {
-    this.twoListService.firstList('', 20, this.listParameter).subscribe(response => {
+    this.twoListService.firstList('', STEP_SIZE, this.listParameter).subscribe(response => {
       this.firstList = response.objects;
       this.firstListCount = response.meta.total_count;
+      this.firstListNextUrl = response.meta.next;
     });
 
-    this.twoListService.secondList('', 20, this.listParameter).subscribe(response => {
+    this.twoListService.secondList('', STEP_SIZE, this.listParameter).subscribe(response => {
       this.secondList = response.objects;
       this.secondListCount = response.meta.total_count;
+      this.secondListNextUrl = response.meta.next;
     });
+  }
+
+  public loadMoreFirstList() {
+    if (this.firstListNextUrl) {
+      this.twoListService.firstList(this.firstListNextUrl, STEP_SIZE, this.listParameter).subscribe(response => {
+        this.firstList.push(response.objects);
+        this.firstListNextUrl = response.meta.next;
+      });
+    }
+  }
+
+  public loadMoreSecondList() {
+    if (this.secondListNextUrl) {
+      this.twoListService.secondList(this.secondListNextUrl, STEP_SIZE, this.listParameter).subscribe(response => {
+        this.secondList.push(response.objects);
+        this.secondListNextUrl = response.meta.next;
+      });
+    }
   }
 
   public ngOnDestroy(): any {
   }
-
-} 
+}
