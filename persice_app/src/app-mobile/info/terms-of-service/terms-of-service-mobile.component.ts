efa985d39e384f596a73450c03261707f94265a0
Ljubomir Simin <ljubomir@coingcs.com>
2016-06-17 14:35:36 +0200

    ICE-2071 Change routes to info pages.

--- persice_app/src/app-mobile/info/terms-of-service/terms-of-service-mobile.component.ts
+++ persice_app/src/app-mobile/info/terms-of-service/terms-of-service-mobile.component.ts
@@ -1,8 +1,11 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {RouterLink} from '@angular/router-deprecated';
+
 
 @Component({
   selector: 'prs-mobile-terms-of-service',
   template: require('./terms-of-service-mobile.html'),
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  directives: [RouterLink]
 })
 export class TermsOfServiceMobileComponent { }
