9151ea0c3e5a47c739064483ef389b6683c0cd60
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-06-27 20:50:01 +0200

    ICE-2132 Refactored selected-person reducer
    - removed unnecessary 'useAsNewConversationRecipient' state

--- persice_app/src/common/actions/selected-person.action.ts
+++ persice_app/src/common/actions/selected-person.action.ts
@@ -6,7 +6,6 @@ export class SelectedPersonActions {
 
   public static SET = '[Selected Person] Set';
   public static CLEAR= '[Selected Person] Clear';
-  public static USE_AS_NEW_CONVERSATION_RECIPIENT = '[Selected Person] Use As New Conversation Recipient';
   public static ACCEPTED = '[Selected Person] Accepted';
   public static PASSED = '[Selected Person] Passed';
 
@@ -20,13 +19,6 @@ export class SelectedPersonActions {
   public clear(): Action {
     return {
       type: SelectedPersonActions.CLEAR,
-      payload: {}
-    };
-  }
-
-  public useAsNewConversationRecipient(): Action {
-    return {
-      type: SelectedPersonActions.USE_AS_NEW_CONVERSATION_RECIPIENT,
       payload: {}
     };
   }
