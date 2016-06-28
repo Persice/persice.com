18bbc7c3ea08a723cb4e3a5c58b8f9561e732741
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-06-28 11:43:35 +0200

    ICE-2133 Migrate frontend routing for signup app (responsive)

---
+++ persice_app/src/signup-mobile/signup-mobile.routes.ts
@@ -0,0 +1,38 @@
+import {provideRouter, RouterConfig} from '@angular/router';
+
+import {SignupInterestsMobileComponent} from './interests';
+import {SignupOffersMobileComponent} from './offers';
+import {SignupGoalsMobileComponent} from './goals';
+import {SignupConnectSocialAccountsMobileComponent} from './connect-social-accounts';
+
+const rootRoutes: RouterConfig = [
+  {
+    path: '',
+    redirectTo: '/interests',
+    terminal: true
+  },
+  {
+    path: 'interests',
+    component: SignupInterestsMobileComponent
+  },
+  {
+    path: 'goals',
+    component: SignupGoalsMobileComponent
+  },
+  {
+    path: 'offers',
+    component: SignupOffersMobileComponent
+  },
+  {
+    path: 'connect',
+    component: SignupConnectSocialAccountsMobileComponent
+  }
+];
+
+export const routesSignupMobile: RouterConfig = [
+  ...rootRoutes
+];
+
+export const SIGNUP_MOBILE_ROUTER_PROVIDERS = [
+  provideRouter(routesSignupMobile)
+];
