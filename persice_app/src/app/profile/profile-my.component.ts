66adf548d7c80dcf50ccd3a57071bd106327aa1c
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-06-29 15:11:41 +0200

    fix(profile): Replaced back functionality with window.history.back()

--- persice_app/src/app/profile/profile-my.component.ts
+++ persice_app/src/app/profile/profile-my.component.ts
@@ -291,9 +291,9 @@ export class ProfileMyComponent implements OnInit, OnDestroy {
   closeProfile(event) {
     let uri = this.historyService.getPrev();
     if (uri !== '') {
-      this._router.parent.navigateByUrl(uri);
+      window.history.back(-1);
     } else {
-      this._router.parent.navigateByUrl('/');
+      window.history.back(-2);
     }
 
   }
