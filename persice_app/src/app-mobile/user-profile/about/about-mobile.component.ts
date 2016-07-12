8514b536fa104712fdca669869d5efd565a14582
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-07-12 17:24:24 +0200

    ICE-2192 Create view more component

--- persice_app/src/app-mobile/user-profile/about/about-mobile.component.ts
+++ persice_app/src/app-mobile/user-profile/about/about-mobile.component.ts
@@ -1,39 +1,13 @@
-// TOOD: make this component generic because it is used in more than one place throughout the app
 import {Component, Input, ChangeDetectionStrategy } from '@angular/core';
-
-const SHOW_CHAR_LIMIT: number = 80;
+import {ViewMoreComponent} from '../../../common/view-more';
 
 @Component({
   selector: 'prs-mobile-profile-about-me',
   template: <any>require('./about-mobile.html'),
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AboutMobileComponent {
-  @Input() type: string;
+export class AboutMobileComponent extends ViewMoreComponent {
   @Input() set about(value: string) {
-    this._setValue(value);
-  }
-
-  // About me less and more contents
-  aboutLess: string = '';
-  aboutMore: string = '';
-
-  // Flag for showing more about me
-  showMore: boolean = false;
-
-  constructor() {}
-
-  public toggleMore(value) {
-    this.showMore = value;
-  }
-
-  private _setValue(value: string) {
-    let contents = value !== null ? value : '';
-    if (contents.length > SHOW_CHAR_LIMIT) {
-      this.aboutLess = contents.substring(0, SHOW_CHAR_LIMIT);
-      this.aboutMore = contents.substring(SHOW_CHAR_LIMIT, contents.length).trim();
-    } else {
-      this.aboutLess = contents;
-    }
+    this.setInitialState(value);
   }
 }
