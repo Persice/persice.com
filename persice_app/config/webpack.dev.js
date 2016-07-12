39b577cf349fd61fb83fbfcd24b80e7246a1fcb9
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-07-12 17:31:51 +0200

    Changed webpack devtool to include source maps

--- persice_app/config/webpack.dev.js
+++ persice_app/config/webpack.dev.js
@@ -38,7 +38,7 @@ module.exports = webpackMerge(commonConfig, {
   //
   // See: http://webpack.github.io/docs/configuration.html#devtool
   // See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
-  devtool: 'eval',
+  devtool: 'eval-cheap-module-source-map',
 
   // Options affecting the output of the compilation.
   //
