66ffe00f41b11026b2d0dd176067ba934b46ac78
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-07-13 19:31:51 +0300

    Automatic commit before deploying frontend

--- bekindred/static/dist/main-mobile.5b8d95564e478425e8cc3fe09c4b3f2c35535c19.prod.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],[function(n,t,e){"use strict";function o(n){return r.bootstrap(p.AppMobileComponent,c.PLATFORM_PROVIDERS_MAIN_MOBILE.concat(u.ENV_PROVIDERS,p.APP_PROVIDERS,[a.provideStore(i.default),d.default])).catch(function(n){return console.error(n)})}var r=e(154),c=e(224),u=e(225),a=e(48),i=e(71),d=e(42),p=e(506);t.main=o;document.addEventListener("DOMContentLoaded",function(){return o()})}]);
-//# sourceMappingURL=main-mobile.5b8d95564e478425e8cc3fe09c4b3f2c35535c19.prod.bundle.map
