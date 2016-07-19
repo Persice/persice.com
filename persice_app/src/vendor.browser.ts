5b64fa31dc471c1e29aec021d909db248c46b94a
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-07-19 12:10:10 +0200

    chore(package.json): ts 2.0.0-beta

--- persice_app/src/vendor.browser.ts
+++ persice_app/src/vendor.browser.ts
@@ -1,7 +1,6 @@
 // For vendors for example jQuery, Lodash, angular2-jwt just import them here unless you plan on
 // chunking vendors files for async loading. You would need to import the async loaded vendors
-// at the entry point of the async loaded file. Also see custom-typings.d.ts as you also need to
-// run `typings install x` where `x` is your module
+// at the entry point of the async loaded file. Also see custom-typings.d.ts
 
 // Angular 2
 import '@angular/platform-browser';
