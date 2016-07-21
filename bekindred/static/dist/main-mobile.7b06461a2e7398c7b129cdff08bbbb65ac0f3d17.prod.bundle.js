5796bc87336e74fdb5eb27ab9f05af28a78f864a
sasha7 <sasa.macakanja@gmail.com>
2016-07-21 11:42:15 +0000

    Automatic commit before deploying frontend

--- bekindred/static/dist/main-mobile.7b06461a2e7398c7b129cdff08bbbb65ac0f3d17.prod.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],[function(n,t,e){"use strict";function o(n){return r.bootstrap(p.AppMobileComponent,c.PLATFORM_PROVIDERS_MAIN_MOBILE.concat(u.ENV_PROVIDERS,p.APP_PROVIDERS,[a.provideStore(i.default),d.default])).catch(function(n){return console.error(n)})}var r=e(154),c=e(228),u=e(229),a=e(48),i=e(71),d=e(42),p=e(510);t.main=o;document.addEventListener("DOMContentLoaded",function(){return o()})}]);
-//# sourceMappingURL=main-mobile.7b06461a2e7398c7b129cdff08bbbb65ac0f3d17.prod.bundle.map
---
+++ bekindred/static/dist/main-mobile.98946e603f63cca51a10e2496f5ee576fce6bb5e.prod.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],[function(n,t,e){"use strict";function o(n){return r.bootstrap(p.AppMobileComponent,c.PLATFORM_PROVIDERS_MAIN_MOBILE.concat(u.ENV_PROVIDERS,p.APP_PROVIDERS,[a.provideStore(i.default),d.default])).catch(function(n){return console.error(n)})}var r=e(154),c=e(228),u=e(229),a=e(48),i=e(71),d=e(42),p=e(510);t.main=o;document.addEventListener("DOMContentLoaded",function(){return o()})}]);
+//# sourceMappingURL=main-mobile.98946e603f63cca51a10e2496f5ee576fce6bb5e.prod.bundle.map
