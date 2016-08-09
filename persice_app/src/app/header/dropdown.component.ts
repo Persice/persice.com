643ca8e10c5312fc664ecde611c1f130d9846e70
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-08-09 21:37:13 +0200

    ICE-2256 Make the entire section clickable around the 'My Account' dropdown in upper right
    - also removed Settings and Signup links

--- persice_app/src/app/header/dropdown.component.ts
+++ persice_app/src/app/header/dropdown.component.ts
@@ -9,12 +9,12 @@ declare var jQuery;
   selector: 'prs-dropdown',
   directives: [DropdownDirective, RouterLink, CheckImageDirective],
   template: `
-  <div class="user-profile">
+  <div class="user-profile" dropdown="#profileDropdown">
     <div class="user-profile__avatar">
       <div class="avatar-holder" checkimage="{{image}}" [suffix]="'.56x56_q100_crop.jpg'" [onchanges]="1">
       </div>
     </div>
-    <div class="user-profile__arrow" dropdown="#profileDropdown">
+    <div class="user-profile__arrow">
       <svg role="img" class="icon">
         <use xlink:href="/static/assets/icons/icons.svg#icon-arrow_down"></use>
       </svg>
@@ -22,8 +22,6 @@ declare var jQuery;
     <div class="dropdown-basic" id="profileDropdown">
       <ul class="list-bare">
         <li><a (click)="openMyProfile($event)">My profile</a></li>
-        <li><a >Settings</a></li>
-        <li><a href="/signup/">Signup</a></li>
         <li><a href="/accounts/logout/">Logout</a></li>
       </ul>
     </div>
