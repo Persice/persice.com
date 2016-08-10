1686c31a5996a811c907f840341e0c2bd6cb3025
sasha7 <sasa.macakanja@gmail.com>
2016-08-10 08:13:34 +0000

    Automatic commit before deploying frontend

--- bekindred/static/dist/main-mobile.1c1d1f2ad031b7e10081662636bd507685a50935.prod.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],[function(n,t,e){"use strict";function o(n){return r.bootstrap(p.AppMobileComponent,c.PLATFORM_PROVIDERS_MAIN_MOBILE.concat(u.ENV_PROVIDERS,p.APP_PROVIDERS,[a.provideStore(i.default),d.default])).catch(function(n){return console.error(n)})}var r=e(155),c=e(229),u=e(230),a=e(48),i=e(71),d=e(42),p=e(511);t.main=o;document.addEventListener("DOMContentLoaded",function(){return o()})}]);
-//# sourceMappingURL=main-mobile.1c1d1f2ad031b7e10081662636bd507685a50935.prod.bundle.map
