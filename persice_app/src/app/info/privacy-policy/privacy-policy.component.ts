7e3348afb9a02ebfc3d2cbfdbe607e4827316747
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-08-11 13:42:30 +0200

    ICE-2252 Scroll to top when component view loads

--- persice_app/src/app/info/privacy-policy/privacy-policy.component.ts
+++ persice_app/src/app/info/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'prs-privacy-policy',
   template: <any>require('./privacy-policy.html'),
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit {
+
+  ngOnInit(): any {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
