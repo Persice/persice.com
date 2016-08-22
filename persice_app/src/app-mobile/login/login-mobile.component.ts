aee596afefcf22c56615cd393c6bab5008f2e27b
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-08-22 14:55:46 +0200

    ICE-2285 Refactored login component

--- persice_app/src/app-mobile/login/login-mobile.component.ts
+++ persice_app/src/app-mobile/login/login-mobile.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth } from 'ng2-ui-auth';
+import { Auth } from '../../common/ng2-ui-auth/auth';
 import { LoginComponent } from '../../common/login/login.component';
 
 @Component({
