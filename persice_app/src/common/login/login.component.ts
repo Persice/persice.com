13c2fbe6c9f0ca9bc231ca9f8932ca510a36bf3f
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-08-22 14:55:46 +0200

    ICE-2285 Refactored login component

--- persice_app/src/common/login/login.component.ts
+++ persice_app/src/common/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Auth } from 'ng2-ui-auth';
+import { Auth } from '../ng2-ui-auth/auth';
 
 export abstract class LoginComponent {
   constructor(protected router: Router, protected auth: Auth) {
@@ -9,12 +9,11 @@ export abstract class LoginComponent {
   public authenticate(provider: string) {
     this.auth.authenticate(provider)
       .subscribe((res) => {
-        setTimeout(() => {
-          this.router.navigateByUrl('/');
-        }, 100);
-
-      }, (err) => {});
-
+        console.log('login res', res);
+        this.router.navigateByUrl('/');
+      }, (err) => {
+        console.log('error', err);
+      });
   }
 
 }
