49450e1822ee05b8255bc6d26dc083ac87f4514b
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-09-07 10:27:53 +0200

    ICE-2348 Added RSVP for event card on desktop

--- persice_app/src/app/events/event-card/event-card.component.ts
+++ persice_app/src/app/events/event-card/event-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Event } from '../../../app-mobile/shared/model/event';
 import { EventCardInsideComponent } from './event-card-inside.component';
+import { RsvpElementComponent } from '../../../common/events/rsvp-element/rsvp-element.component';
 
 @Component({
   selector: 'prs-event-card',
   template: <any>require('./event-card.html'),
-  directives: [EventCardInsideComponent]
+  directives: [EventCardInsideComponent, RsvpElementComponent]
 })
 export class EventCardComponent {
   @Input() event: Event;
