a46fd9c19c699f78a5ef8e78f7f6316348ca0860
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-09-13 01:57:48 +0200

    ICE-2268 Refactored edit about for my profile

--- persice_app/src/app/profile/about.component.ts
+++ persice_app/src/app/profile/about.component.ts
@@ -1,51 +1,20 @@
-import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
-import { IgnoreMarkupPipe } from '../shared/pipes/ignore_markup.pipe';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { ViewMoreComponent } from '../../common/view-more/view-more.component';
 
 @Component({
   selector: 'prs-profile-about',
-  template: `
-  <h4 class="module-title mb0" *ngIf="!editable">About</h4>
-  <h4 class="module-title mb0" *ngIf="editable">
-  <a (click)="openEdit.next('about')" class="edit-link">About <span class="edit-link__icon">
-    <svg role="img" class="icon ">
-      <use xlink:href="/assets/icons/icons.svg#icon-edit_info"></use>
-    </svg>
-    </span></a>
-  </h4>
-
-  <div class="profile-feature" *ngIf="!editable">
-    {{aboutMore | ignoreMarkup}}
-    <a (click)="showMore($event)" *ngIf="!hideMoreLink" class="link-blank">View all</a>
-  </div>
-
-  <div class="profile-feature" *ngIf="editable">
-    <a (click)="openEdit.next('about')" class="edit-link">{{aboutMore | ignoreMarkup}}</a>
-    <a (click)="showMore($event)" *ngIf="!hideMoreLink" class="link-blank">View all</a>
-  </div>
-  `,
-  pipes: [IgnoreMarkupPipe]
+  template: <any>require('./about.html'),
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AboutComponent implements OnChanges {
-  @Input() about;
+export class AboutComponent extends ViewMoreComponent {
+  @Input() set about(value: string) {
+    this.setInitialState(value);
+  };
+
   @Input() editable;
   @Output() openEdit: EventEmitter<any> = new EventEmitter;
-  aboutMore: string = '';
-  hideMoreLink: boolean = true;
 
-  ngOnChanges(values) {
-    if (values.about && values.about.currentValue) {
-      if (values.about.currentValue.length > 120) {
-        this.aboutMore = values.about.currentValue.substring(0, 119) + '...';
-        this.hideMoreLink = false;
-      } else {
-        this.aboutMore = values.about.currentValue;
-        this.hideMoreLink = true;
-      }
-    }
-  }
-
-  showMore() {
-    this.hideMoreLink = true;
-    this.aboutMore = this.about;
+  constructor() {
+    super(100);
   }
 }
