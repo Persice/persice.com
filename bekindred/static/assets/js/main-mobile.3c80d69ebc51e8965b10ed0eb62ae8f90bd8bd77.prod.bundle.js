1ef6eade2fc5a42f6056fcb77026966f2cb27c2c
Sasa Macakanja <sasa.macakanja@gmail.com>
2016-09-23 15:02:59 +0000

    Automatic commit before deploying frontend

---
+++ bekindred/static/assets/js/main-mobile.3c80d69ebc51e8965b10ed0eb62ae8f90bd8bd77.prod.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],[function(n,t,e){"use strict";function o(n){return r.bootstrap(p.AppMobileComponent,c.PLATFORM_PROVIDERS_MAIN_MOBILE.concat(u.ENV_PROVIDERS,p.APP_PROVIDERS,[a.provideStore(i.default),d.default])).catch(function(n){return console.error(n)})}var r=e(323),c=e(576),u=e(577),a=e(44),i=e(65),d=e(41),p=e(500);t.main=o;document.addEventListener("DOMContentLoaded",function(){return o()})}]);
+//# sourceMappingURL=main-mobile.3c80d69ebc51e8965b10ed0eb62ae8f90bd8bd77.prod.bundle.map
