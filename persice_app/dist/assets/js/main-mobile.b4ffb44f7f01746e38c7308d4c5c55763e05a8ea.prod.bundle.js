1fa5c3206684862c1d1fdc75bacfeefe0a154d43
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-09-27 20:59:00 +0300

    Automatic commit before deploying frontend

---
+++ persice_app/dist/assets/js/main-mobile.b4ffb44f7f01746e38c7308d4c5c55763e05a8ea.prod.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],[function(n,t,o){"use strict";function e(n){return r.bootstrap(a.AppMobileComponent,c.PLATFORM_PROVIDERS_MAIN_MOBILE.concat(u.ENV_PROVIDERS,a.APP_PROVIDERS)).catch(function(n){return console.error(n)})}var r=o(325),c=o(582),u=o(583),a=o(505);t.main=e;document.addEventListener("DOMContentLoaded",function(){return e()})}]);
+//# sourceMappingURL=main-mobile.b4ffb44f7f01746e38c7308d4c5c55763e05a8ea.prod.bundle.map
